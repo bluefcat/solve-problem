5745d6b7584c13396ee0420f467ac3e15e8a2f77
bluefcat <bluefcat@gmail.com>
2024-01-31 15:50:00 +0900

    Fix some format

--- test.sh
+++ test.sh
@@ -5,10 +5,13 @@ printf "[%s is compiling now]\n" $target
 gcc $target -O2 -Wall -lm -static -std=gnu99 -o ./a.out
 
 input_list=$(find . -name $(printf "%s_*.txt" $1))
-
+idx=0
 for input in $input_list
 do
-	echo $(./a.out < $input) 
+	printf "TESTCAST #%s\n>>>>>\n" $idx
+	cat $input
+	printf "<<<<<\n%s\n\n" $(./a.out < $input) 
+	idx=$(($idx+1))
 done
-echo ''
+
 

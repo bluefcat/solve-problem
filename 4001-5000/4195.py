e3b21805674083603459b6f57a49e78bec761bce
bluefcat <bluefcat@gmail.com>
2024-06-27 13:59:07 +0900

    Mod solve file in 240627

--- 4001-5000/4195.py
+++ 4001-5000/4195.py
@@ -1,39 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-def get_parent(d: dict, name: str) -> str:
+def get_parent(d: list, name: int) -> int:
     if d[name] == name:
         return name
 
     d[name] = get_parent(d, d[name])
     return d[name]
 
-def union(d: dict, x: str, y: str) -> None:
+def union(d: list, n: list, x: int, y: int) -> None:
     px = get_parent(d, x)
     py = get_parent(d, y)
-    target = py
+    if px == py:
+        return
+    p, target = px, py
     if px < py:
-        target = px
+        p, target = py, px
     
-    d[px] = target
-    d[py] = target
+    d[p] = target
+    n[target] += n[p]
 
 def main():
     t = int(input().strip()) 
 
     while t:
         t -= 1
-        network = {}
+        f = {}
+        cnt = 0
+        network = [i for i in range(100001)]
+        fc = [1 for _ in range(100001)]
         n = int(input().strip())
         for _ in range(n):
             x, y = input().strip().split() 
-            if x not in network:
-                network[x] = x
-            if y not in network:
-                network[y] = y
-            union(network, x, y)
-            
-            root = get_parent(network, x)
-            print(sum(get_parent(network,y) == root for _, y in network.items()))
+            f[x] = f.get(x, cnt); cnt += 1
+            f[y] = f.get(y, cnt); cnt += 1
+            union(network, fc, f[x], f[y])
+            root = get_parent(network, f[x])
+            print(fc[root])
+
 
 main()

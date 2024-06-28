62970729bdd702ccffedfe1d2f2ff5ef16758408
bluefcat <bluefcat@gmail.com>
2024-06-28 14:55:30 +0900

    Mod solve file in 240628

--- 1001-2000/1167.cpp
+++ 1001-2000/1167.cpp
@@ -1,59 +1,70 @@
 #include <cstdio>
+#include <unordered_map>
+#include <queue>
 #include <algorithm>
-#include <map>
-#include <stack>
+
+template<typename T>
+std::pair<int, long long> get_start(
+	T& tree,
+	std::unordered_map<int, bool>& check,
+	int x
+){
+	std::pair<int, long long> result{x, 0};
+	check[x] = true;
+	for(const auto& [y, cost]: tree[x]){
+		if(check[y]) continue;
+		auto p = get_start(tree, check, y);
+		if(result.second < p.second + cost){
+			result = std::make_pair(p.first, p.second + cost);
+		}
+	}
+	return result;
+}
+
+template<typename T>
+long long visit(
+	T& tree,
+	std::unordered_map<int, bool>& check,
+	int x
+){
+	long long result = 0;
+	check[x] = true;
+	for(const auto& [y, cost]: tree[x]){
+		if(check[y]) continue;
+		result = std::max(result, visit(tree, check, y) + cost);
+	}
+	return result;
+}
+
 
 int main(){
-	int result = 0;
+	std::unordered_map<
+		int,
+		std::unordered_map<int, int>
+	> tree;
+	
 	int n;
 	scanf("%d", &n);
-	
-	std::multimap<int, std::pair<int, int>> tre;
-	std::map<int, bool> visited;
-	std::stack<std::pair<int, int>> next_visited;
 
 	for(int i = 0; i < n; i ++){
-		int v;
-		scanf("%d", &v);
-		
-		visited.insert({v, false});
-
-		int x[2] = { 0, };
-		int idx = 0;
-
-		while(scanf("%d", x+idx) && x[idx] != -1){
-			if(idx == 1){
-				tre.insert({v, {x[0], x[1]}});
-			}
-			idx = (idx + 1) % 2;
-		}
-		
+		int x, y, t, flag = 0;
+		scanf("%d", &x);
+		while(scanf("%d", &t) && t != -1){
+			if(flag == 0)
+				y = t;
+			else
+				tree[x][y] = t;
+			flag = (flag + 1) %2;
+		}	
 	}
+	std::queue<int> q;
 	
-	for(int i = 1; i <= n; i ++){
-		next_visited.push({i, 0});
-		
-		while(!next_visited.empty()){
-			auto p = next_visited.top(); next_visited.pop();
-			int x = p.first;
-			int s = p.second;
-			result = std::max(s, result);
-			visited[x] = true;
+	long long result = 0;
+	std::unordered_map<int, bool> check;
+	auto idx = get_start(tree, check, tree.begin()->first);
+	check.clear();
+	result = visit(tree, check, idx.first);
+	printf("%lld\n", result);
 
-
-			auto range = tre.equal_range(x);
-			for(auto& pair = range.first; pair != range.second; ++pair){
-				int next = pair->second.first;
-				int dist = pair->second.second;
-				
-				if(visited[next]) continue;
-
-				next_visited.push({next, s+dist});
-			}
-		}
-	}
-
-
-	printf("%d\n", result);
 	return 0;
 }

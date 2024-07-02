ec7825ddf2a3d7acc60fb497a8edd62a982087a2
bluefcat <bluefcat@gmail.com>
2024-07-02 17:49:49 +0900

    Mod solve file in 240702

--- 31001-32000/31853.cpp
+++ 31001-32000/31853.cpp
@@ -3,6 +3,8 @@
 #include <unordered_map>
 #include <algorithm>
 
+#include <limits.h>
+
 struct Compare{
 	bool operator()(
 		std::pair<int, int> x, 
@@ -26,7 +28,24 @@ long long init(
 	int midx = (sidx + eidx) >> 1;
 	auto l = init(tree, sidx, midx, get_left(idx));
 	auto r = init(tree, midx+1, eidx, get_right(idx));
-	return tree[idx] = l + r;
+	return tree[idx] = l+r;
+}
+
+template<typename D, typename T>
+std::pair<int, int> get_range(
+	const D& line,	
+	T& tree,
+	int sidx, int eidx,
+	int x1, int x2,
+	int idx
+){
+	if(x2 < line[sidx].first || line[eidx].first < x1) return {INT_MAX, -1};
+	if(x1 <= line[sidx].first && line[eidx].first <= x2) return {sidx, eidx};
+
+	int midx = (sidx + eidx) >> 1;
+	auto l = get_range(line, tree, sidx, midx, x1, x2, get_left(idx));
+	auto r = get_range(line, tree, midx+1, eidx, x1, x2, get_right(idx));
+	return {std::min(l.first, r.first), std::max(l.second, r.second)};
 }
 
 template<typename D, typename T>
@@ -34,21 +53,20 @@ long long sum(
 	const D& line,	
 	T& tree,
 	int sidx, int eidx,
-	int x1, int x2, int y,
+	int le, int ri,
+	int y,
 	int idx
 ){
-	if(x2 < line[sidx].first || line[eidx].first < x1) return 0;
-	if(line[sidx].second < y && line[eidx].second < y) return 0;
-	if(
-		x1 <= line[sidx].first && line[eidx].first <= x2 &&
-		y <= line[sidx].second && y <= line[eidx].second
-	) return tree[idx];
+	if(ri < sidx || eidx < le) return 0;
+	if(sidx == eidx) return (long long)(y <= line[sidx].second);
+	if(y <= line[sidx].second && y <= line[eidx].second) return tree[idx];
 
 	int midx = (sidx + eidx) >> 1;
-	long long l = sum(line, tree, sidx, midx, x1, x2, y, get_left(idx));
-	long long r = sum(line, tree, midx+1, eidx, x1, x2, y, get_right(idx));
+	auto l = sum(line, tree, sidx, midx, le, ri, y, get_left(idx));
+	auto r = sum(line, tree, midx+1, eidx, le, ri, y, get_right(idx));
 	return l+r;
 }
+
 
 int main(){
 	int n, m;
@@ -76,8 +94,10 @@ int main(){
 	for(const auto& [x1, y1]: line){
 		for(const auto& [x2, y2]: line){
 			if(!(x1 < x2 && x2 < y1 && y1 < y2)) continue;
-
-			result += sum(line, tree, 0, line.size()-1, x2+1, y1-1, y2+1, 0);
+			//y2 < y3
+			auto r = get_range(line, tree, 0, line.size()-1, x2+1, y1-1, 0);
+			if(r.second == -1) continue;
+			result += sum(line, tree, 0, line.size()-1,r.first, r.second, y2+1, 0);
 		}
 	}
 

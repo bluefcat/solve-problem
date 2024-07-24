2800bba0dab6e6c183013c5ae83863955522620c
bluefcat <bluefcat@gmail.com>
2024-07-24 15:27:17 +0900

    Add solve file in 240724

---
+++ 1001-2000/1018.cpp
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <cmath>
+#include <climits>
+
+constexpr int N = 57;
+
+int main(){
+	int W, H;
+	bool board[N][N] = { 0, };
+	int cache[2][N][N] = { 0, };
+	scanf("%d %d", &H, &W);
+	
+	for(int i = 0; i < H; i ++){
+		for(int j = 0; j < W; j ++){
+			char x;
+			scanf("\n%c", &x);
+			board[i][j] = (x == 'B');
+		}
+	}
+
+	for(int c = 0; c <= 1; c ++){
+		int b = c;
+		for(int i = 0; i < H; i ++){
+			for(int j = 0; j < W; j ++){
+				b ^= true;
+				cache[c][i+1][j+1] += std::abs(b - board[i][j]);
+			}
+			if(!(W % 2)) b ^= true;
+		}
+	}	
+
+	for(int c = 0; c <= 1; c ++){
+		for(int i = 0; i < H+1; i ++){
+			for(int j = 0; j < W+1; j ++){
+				cache[c][i][j] += cache[c][i][j-1];
+			}
+		}
+		for(int i = 0; i < H+1; i ++){
+			for(int j = 0; j < W+1; j ++){
+				cache[c][i][j] += cache[c][i-1][j];
+			}
+		}
+	}
+
+
+	int result = INT_MAX;
+	for(int c = 0; c <= 1; c ++){
+		for(int i = 8; i < H+1; i ++){
+			for(int j = 8; j < W+1; j ++){
+				result = std::min(
+					result,
+					cache[c][i][j] - 
+					(cache[c][i][j-8] + cache[c][i-8][j] - cache[c][i-8][j-8])
+				);
+			}
+		}
+	}
+
+
+	printf("%d\n", result);
+
+	return 0;
+};

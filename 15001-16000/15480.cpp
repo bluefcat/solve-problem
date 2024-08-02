982c479b221632b44592c477cc828c02cac6cf58
bluefcat <bluefcat@gmail.com>
2024-08-02 13:08:44 +0900

    Add solve file in 240802

--- 15001-16000/15480.cpp
+++ 15001-16000/15480.cpp
@@ -19,7 +19,37 @@ void build_depth(T& graph, int* depth, int table[][N], int root, int idx){
 	return;
 }
 
+int lca(int* depth, int table[][N], int u, int v){
+	int du = depth[u];
+	int dv = depth[v];
+	
+	if(du > dv){ 
+		int tmp = du; du = dv; dv = tmp; 
+		tmp = u; u = v; v = tmp;
+	}
+	
+	int diff = dv - du;	
+	for(int idx = LN-1; idx >= 0; idx --){
+		if((diff & (1 << idx)) == (1 << idx)) {
+			v = table[idx][v];
+		}
+	}
+	
+	if(u != v){
+		for(int idx = LN-1; idx >= 0; idx--){
+			if(table[idx][u] != 0 && table[idx][u] != table[idx][v]){
+				u = table[idx][u];
+				v = table[idx][v];
+			}
+		}
+		u = table[0][u];
+	}
+	return u;	
+}
+
 int main(){
+	int depth[N] = { 0, };
+	int table[LN][N] = { 0, };
 	unordered_map<int, vector<int>> graph{};	
 	int n = 0;
 
@@ -32,46 +62,22 @@ int main(){
 		graph[v].push_back(u);
 	}
     
+	build_depth(graph, depth, table, 1, 1);
+	for(int i = 1; i < LN; i ++)
+		for(int x = 1; x <= n; x ++)
+			if(table[i-1][x] != 0)
+				table[i][x] = table[i-1][table[i-1][x]];	
+
 	int m = 0;
 	scanf("%d", &m);
 	for(int i = 0; i < m; i ++){
 		int t, u, v;
 		scanf("%d %d %d", &t, &u, &v);
-        
-	    int depth[N] = { 0, };
-	    int table[LN][N] = { 0, };
-	    build_depth(graph, depth, table, t, t);
-	
-	    for(int i = 1; i < LN; i ++)
-		    for(int x = 1; x <= n; x ++)
-			    if(table[i-1][x] != 0)
-				    table[i][x] = table[i-1][table[i-1][x]];	
 		
-		int du = depth[u];
-		int dv = depth[v];
-		
-		if(du > dv){ 
-			int tmp = du; du = dv; dv = tmp; 
-			tmp = u; u = v; v = tmp;
-		}
-		
-		int diff = dv - du;	
-		for(int idx = LN-1; idx >= 0; idx --){
-			if((diff & (1 << idx)) == (1 << idx)) {
-				v = table[idx][v];
-			}
-		}
-		
-		if(u != v){
-			for(int idx = LN-1; idx >= 0; idx--){
-				if(table[idx][u] != 0 && table[idx][u] != table[idx][v]){
-					u = table[idx][u];
-					v = table[idx][v];
-				}
-			}
-			u = table[0][u];
-		}
-		printf("%d\n", u);
+		printf("%d\n", lca(depth, table, t, u)^
+					   lca(depth, table, t, v)^
+					   lca(depth, table, u, v)
+		);
 	}
 
 	return 0;

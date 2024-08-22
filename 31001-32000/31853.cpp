12d1760b70cc62a5d795680072c290f38fbcf9aa
bluefcat <bluefcat@gmail.com>
2024-08-22 11:23:59 +0900

    Add solve file in 240822

--- 31001-32000/31853.cpp
+++ 31001-32000/31853.cpp
@@ -1,106 +1,73 @@
 #include <cstdio>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
 
-#include <limits.h>
+using std::pair;
+using std::vector;
+using ll = long long;
 
-struct Compare{
-	bool operator()(
-		std::pair<int, int> x, 
-		std::pair<int, int> y){
-		return x.first > y.first;
-	}
-};
+constexpr int N = 20001;
+constexpr int get_left(int x){ return (x << 1) + 1; }
+constexpr int get_right(int x){ return (x << 1) + 2; }
 
-static inline int get_left(int x){ return (x << 1) + 1; }
-static inline int get_right(int x){ return (x << 1) + 2; }
-
-template<typename T>
-long long init(
-	T& tree,	
-	int sidx, int eidx,
-	int idx
-){
-	if(sidx == eidx){
-		return tree[idx] = 1;
-	}
-	int midx = (sidx + eidx) >> 1;
-	auto l = init(tree, sidx, midx, get_left(idx));
-	auto r = init(tree, midx+1, eidx, get_right(idx));
-	return tree[idx] = l+r;
+bool compare(pair<int, int>& x, pair<int, int>& y){
+	if(x.second == y.second) 
+		return x.first < y.first;
+	return x.second < y.second;
 }
 
-template<typename D, typename T>
-std::pair<int, int> get_range(
-	const D& line,	
-	T& tree,
-	int sidx, int eidx,
-	int x1, int x2,
-	int idx
-){
-	if(x2 < line[sidx].first || line[eidx].first < x1) return {INT_MAX, -1};
-	if(x1 <= line[sidx].first && line[eidx].first <= x2) return {sidx, eidx};
-
-	int midx = (sidx + eidx) >> 1;
-	auto l = get_range(line, tree, sidx, midx, x1, x2, get_left(idx));
-	auto r = get_range(line, tree, midx+1, eidx, x1, x2, get_right(idx));
-	return {std::min(l.first, r.first), std::max(l.second, r.second)};
+ll update(ll tree[], int l, int r, int k, int idx){
+	if(!(l <= k && k <= r)) return tree[idx];
+	if(l == r) return tree[idx] += 1;
+	int m = (l + r) >> 1;
+	return tree[idx] = update(tree, l, m, k, get_left(idx)) +
+					   update(tree, m+1, r, k, get_right(idx));
 }
 
-template<typename D, typename T>
-long long sum(
-	const D& line,	
-	T& tree,
-	int sidx, int eidx,
-	int le, int ri,
-	int y,
-	int idx
-){
-	if(ri < sidx || eidx < le) return 0;
-	if(sidx == eidx) return (long long)(y <= line[sidx].second);
-	if(y <= line[sidx].second && y <= line[eidx].second) return tree[idx];
-
-	int midx = (sidx + eidx) >> 1;
-	auto l = sum(line, tree, sidx, midx, le, ri, y, get_left(idx));
-	auto r = sum(line, tree, midx+1, eidx, le, ri, y, get_right(idx));
-	return l+r;
+ll query(ll tree[], int l, int r, int s, int e, int idx){
+	if(r <= s | e <= l) return 0;
+	if(s < l && r < e) return tree[idx];
+	int m = (l + r) >> 1;
+	return query(tree, l, m, s, e, get_left(idx)) + 
+		   query(tree, m+1, r, s, e, get_right(idx));
 }
 
 
 int main(){
 	int n, m;
-	std::vector<std::pair<int, int>> line;
-	std::unordered_map<int, long long> tree;
-
-	long long result = 0;
+	vector<int> dict{};
+	vector<pair<int, int>> line{};
 	scanf("%d %d", &n, &m);
-	
+
 	for(int i = 0; i < m; i ++){
-		int x, y;
-		scanf("%d %d", &x, &y);
-		line.push_back(
-			{std::min(x, y), std::max(x, y)}
-		);
+		int u, v;
+		scanf("%d %d", &u, &v);
+		if(u > v){
+			int tmp = u; u = v; v = tmp;
+		}
+		dict.push_back(u);
+		dict.push_back(v);
+
+		line.push_back({u, v});
 	}
-	
-	std::sort(
-		line.begin(), 
-		line.end(), [](const auto& x, const auto& y){
-			return x.first < y.first;
-	});
-	
-	init(tree, 0, line.size()-1, 0);
-	for(const auto& [x1, y1]: line){
-		for(const auto& [x2, y2]: line){
-			if(!(x1 < x2 && x2 < y1 && y1 < y2)) continue;
-			//y2 < y3
-			auto r = get_range(line, tree, 0, line.size()-1, x2+1, y1-1, 0);
-			if(r.second == -1) continue;
-			result += sum(line, tree, 0, line.size()-1,r.first, r.second, y2+1, 0);
+	std::sort(dict.begin(), dict.end());
+	std::sort(line.begin(), line.end(), compare);
+
+	ll result = 0;
+	for(int i = 0; i < line.size(); i ++){
+		auto& [u, v] = line[i];
+		int u_id = std::upper_bound(dict.begin(), dict.end(), u) - dict.begin();
+		ll tree[4*N+4] = { 0, };
+		for(int j = i-1; j >=0; j --){
+			auto& [p, q] = line[j];
+			if(!((p < u) && (u < q && q < v))) continue;
+			int p_id = std::upper_bound(dict.begin(), dict.end(), p) - dict.begin();
+			ll pl = query(tree, 0, 2*m, p_id, u_id, 0);
+			result += pl;
+			update(tree, 0, 2*m, p_id, 0);
 		}
 	}
-
 	printf("%lld\n", result);
 	return 0;
+
 }

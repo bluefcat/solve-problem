1dcd75c3cd0616358f709f672e85da31acb4ca26
bluefcat <bluefcat@gmail.com>
2024-08-22 13:22:49 +0900

    Mod solve file in 240822

--- 31001-32000/31853.cpp
+++ 31001-32000/31853.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <cstdio>
 #include <vector>
 #include <algorithm>
@@ -6,11 +7,11 @@ using std::pair;
 using std::vector;
 using ll = long long;
 
-constexpr int N = 20001;
+constexpr int N = 10001;
 constexpr int get_left(int x){ return (x << 1) + 1; }
 constexpr int get_right(int x){ return (x << 1) + 2; }
 
-bool compare(pair<int, int>& x, pair<int, int>& y){
+bool compare(pair<ll, ll>& x, pair<ll, ll>& y){
 	if(x.second == y.second) 
 		return x.first < y.first;
 	return x.second < y.second;
@@ -25,7 +26,7 @@ ll update(ll tree[], int l, int r, int k, int idx){
 }
 
 ll query(ll tree[], int l, int r, int s, int e, int idx){
-	if(r <= s | e <= l) return 0;
+	if(r <= s || e <= l) return 0;
 	if(s < l && r < e) return tree[idx];
 	int m = (l + r) >> 1;
 	return query(tree, l, m, s, e, get_left(idx)) + 
@@ -34,16 +35,19 @@ ll query(ll tree[], int l, int r, int s, int e, int idx){
 
 
 int main(){
-	int n, m;
-	vector<int> dict{};
-	vector<pair<int, int>> line{};
-	scanf("%d %d", &n, &m);
-
-	for(int i = 0; i < m; i ++){
-		int u, v;
-		scanf("%d %d", &u, &v);
+	ll n, m;
+	vector<ll> dict{};
+	vector<pair<ll, ll>> line{};
+	scanf("%lld %lld", &n, &m);
+	assert(2 <= n && n <= 1000000000);
+	for(ll i = 0; i < m; i ++){
+		ll u, v;
+		scanf("%lld %lld", &u, &v);
+		assert(u != v);
+		assert(u <= n);
+		assert(v <= n);
 		if(u > v){
-			int tmp = u; u = v; v = tmp;
+			ll tmp = u; u = v; v = tmp;
 		}
 		dict.push_back(u);
 		dict.push_back(v);
@@ -54,7 +58,7 @@ int main(){
 	std::sort(line.begin(), line.end(), compare);
 
 	ll result = 0;
-	for(int i = 0; i < line.size(); i ++){
+	for(int i = 2; i < line.size(); i ++){
 		auto& [u, v] = line[i];
 		int u_id = std::upper_bound(dict.begin(), dict.end(), u) - dict.begin();
 		ll tree[4*N+4] = { 0, };
@@ -62,9 +66,9 @@ int main(){
 			auto& [p, q] = line[j];
 			if(!((p < u) && (u < q && q < v))) continue;
 			int p_id = std::upper_bound(dict.begin(), dict.end(), p) - dict.begin();
-			ll pl = query(tree, 0, 2*m, p_id, u_id, 0);
+			ll pl = query(tree, 0, N-1, p_id, u_id, 0);
 			result += pl;
-			update(tree, 0, 2*m, p_id, 0);
+			update(tree, 0, N-1, p_id, 0);
 		}
 	}
 	printf("%lld\n", result);

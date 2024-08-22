30e047f587f8995d6305a122d1a0fd743f1cac24
bluefcat <bluefcat@gmail.com>
2024-08-22 17:17:21 +0900

    Mod solve file in 240822

--- 31001-32000/31853.cpp
+++ 31001-32000/31853.cpp
@@ -8,83 +8,70 @@ using std::pair;
 using std::vector;
 using ll = long long;
 
-constexpr int N = 14001;
-constexpr int get_left(int x){ return (x << 1) + 1; }
-constexpr int get_right(int x){ return (x << 1) + 2; }
+constexpr int N = 7001;
 
 bool compare(pair<int, int>& x, pair<int, int>& y){
-	if(x.second == y.second) 
-		return x.first < y.first;
 	return x.second < y.second;
 }
 
-ll tree[4*N+4][2] = { 0, };
-
-void propagate(int l, int r, int idx){
-	if(tree[idx][1] == 0) return;
-	if(l != r){
-		tree[get_left(idx)][1] = 1;
-		tree[get_right(idx)][1] = 1;
-	}
-	tree[idx][0] = 0;
-	tree[idx][1] = 0;
-}
+ll tree[4*N+4] = { 0, };
 
 ll update(int l, int r, int k, int idx){
-	propagate(l, r, idx);
-	if(!(l <= k && k <= r)) return tree[idx][0];
-	if(l == r) return tree[idx][0] = 1;
-	int m = (l + r) >> 1;
-	return tree[idx][0] = update(l, m, k, get_left(idx)) +
-					   update(m+1, r, k, get_right(idx));
+	ll result = 0;
+	while(l < r){
+		int m = (l + r) >> 1;
+		if(k <= m){
+			result += tree[((idx << 1) + 2)];
+			idx = (idx << 1) + 1;
+			r = m;
+		}
+		else{
+			idx = (idx << 1) + 2;
+			l = m+1;
+		}
+	}
+	result += tree[idx];
+	while(idx != 0){
+		tree[idx] ++;
+		idx = (idx-1) >> 1;
+	}
+	tree[0] ++;
+	return result;
 }
-
-ll query(int l, int r, int s, int e, int idx){
-	propagate(l, r, idx);
-	if(r <= s || e <= l) return 0;
-	if(s < l && r < e) return tree[idx][0];
-	int m = (l + r) >> 1;
-	return query(l, m, s, e, get_left(idx)) + 
-		   query(m+1, r, s, e, get_right(idx));
-}
-
 
 int main(){
 	int n, m;
-	vector<int> dict{};
-	std::unordered_map<int, int> pos{};
+	vector<int> udict{};
+	std::unordered_map<int, int> upos{};
 	vector<pair<int, int>> line{};
-	scanf("%d %d", &n, &m);
+	vector<int> liner{};
+	n = 13998;
+	m = 6999;
+	//scanf("%d %d", &n, &m);
 	for(int i = 0; i < m; i ++){
 		int u, v;
-		scanf("%d %d", &u, &v);
+		u = i+1;
+		v = m+1+i;
+		//scanf("%d %d", &u, &v);
 		if(u > v){
 			ll tmp = u; u = v; v = tmp;
 		}
-		dict.push_back(u);
-		dict.push_back(v);
-
+		udict.push_back(u);
 		line.push_back({u, v});
 	}
-	std::sort(dict.begin(), dict.end());
+	std::sort(udict.begin(), udict.end());
 	std::sort(line.begin(), line.end(), compare);
-
-	for(int i = 0; i < dict.size(); i ++){
-		pos[dict[i]] = i;
-	}
-
+	for(int i = 0; i < m; i ++) upos[udict[i]] = i;
+	
 	ll result = 0;
-	for(int i = 2; i < line.size(); i ++){
-		auto& [u, v] = line[i];
-	    int u_id = pos[u];
-		tree[0][1] = 1;
+	for(int i = 2; i < m; i ++){
+	    int u_id = upos[line[i].first];
+		for(int j = 0; j < 4*N+4; j ++) tree[j] = 0;
 		for(int j = i-1; j >=0; j --){
-			auto& [p, q] = line[j];
-			if(!((p < u) && (u < q && q < v))) continue;
-			int p_id = pos[p];
-			ll pl = query(0, N-1, p_id, u_id, 0);
-			result += pl;
-			update(0, N-1, p_id, 0);
+			int p_id = upos[line[j].first];
+			if(p_id < u_id && line[i].first < line[j].second){ 
+				result += update(0, N, p_id, 0);
+			}
 		}
 	}
 	printf("%lld\n", result);

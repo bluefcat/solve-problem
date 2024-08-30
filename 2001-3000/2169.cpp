70ae608136a4368c054e98c04b81ac8ec2cae22f
bluefcat <bluefcat@gmail.com>
2024-08-30 16:07:45 +0900

    Add solve file in 240830

--- 2001-3000/2169.cpp
+++ 2001-3000/2169.cpp
@@ -13,29 +13,29 @@ int cache[N][N][2] = { 0, };
 int main(){
 	int n, m;
 	scanf("%d %d", &n, &m);
-	for(int i = 0; i < m; i ++)
-		for(int j = 0; j < n; j ++){
+	for(int i = 0; i < n; i ++)
+		for(int j = 0; j < m; j ++){
 			scanf("%d", field[i]+j);
 			cache[i][j][l] = INT_MIN;
 			cache[i][j][r] = INT_MIN;
 		}
 	
 	cache[0][0][r] = field[0][0];
-	for(int i = 1; i < n; i ++){
+	for(int i = 1; i < m; i ++){
 		cache[0][i][r] = cache[0][i-1][r] + field[0][i];
 	}
 
-	for(int i = 1; i < m; i ++){
-		for(int j = 0; j < n; j ++){
+	for(int i = 1; i < n; i ++){
+		for(int j = 0; j < m; j ++){
 			cache[i][j][r] = max(cache[i-1][j][l], cache[i-1][j][r]) + field[i][j];
 			cache[i][j][l] = max(cache[i-1][j][l], cache[i-1][j][r]) + field[i][j];
 		}
-		for(int j = 1; j < n; j ++)
+		for(int j = 1; j < m; j ++)
 			cache[i][j][r] = max(cache[i][j][r], cache[i][j-1][r] + field[i][j]);
-		for(int j = n-2; j >= 0; j --)
+		for(int j = m-2; j >= 0; j --)
 			cache[i][j][l] = max(cache[i][j][l], cache[i][j+1][l] + field[i][j]);
 	}
-		
-	printf("%d\n", max(cache[m-1][n-1][l], cache[m-1][n-1][r]));
+
+	printf("%d\n", max(cache[n-1][m-1][l], cache[n-1][m-1][r]));
 	return 0;
 }

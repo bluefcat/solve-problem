661dafbd1bbd12634d56a1f7ea15d8114faa5761
bluefcat <bluefcat@gmail.com>
2024-08-30 16:00:40 +0900

    Add solve file in 240830

---
+++ 2001-3000/2169.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <climits>
+#include <algorithm>
+
+using std::max;
+
+constexpr int N = 1001;
+constexpr int l = 0;
+constexpr int r = 1;
+int field[N][N] = { 0, };
+int cache[N][N][2] = { 0, };
+
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	for(int i = 0; i < m; i ++)
+		for(int j = 0; j < n; j ++){
+			scanf("%d", field[i]+j);
+			cache[i][j][l] = INT_MIN;
+			cache[i][j][r] = INT_MIN;
+		}
+	
+	cache[0][0][r] = field[0][0];
+	for(int i = 1; i < n; i ++){
+		cache[0][i][r] = cache[0][i-1][r] + field[0][i];
+	}
+
+	for(int i = 1; i < m; i ++){
+		for(int j = 0; j < n; j ++){
+			cache[i][j][r] = max(cache[i-1][j][l], cache[i-1][j][r]) + field[i][j];
+			cache[i][j][l] = max(cache[i-1][j][l], cache[i-1][j][r]) + field[i][j];
+		}
+		for(int j = 1; j < n; j ++)
+			cache[i][j][r] = max(cache[i][j][r], cache[i][j-1][r] + field[i][j]);
+		for(int j = n-2; j >= 0; j --)
+			cache[i][j][l] = max(cache[i][j][l], cache[i][j+1][l] + field[i][j]);
+	}
+		
+	printf("%d\n", max(cache[m-1][n-1][l], cache[m-1][n-1][r]));
+	return 0;
+}

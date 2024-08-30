490d8035081614672814153db1191d10eeb959e2
bluefcat <bluefcat@gmail.com>
2024-08-30 09:44:25 +0900

    Add solve file in 240830

---
+++ 17001-18000/17412.cpp
@@ -0,0 +1,62 @@
+#include <cstdio>
+#include <unordered_map>
+#include <queue>
+
+using std::unordered_map;
+using std::queue;
+
+constexpr int N = 501;
+
+int main(){
+	unordered_map<
+		int, unordered_map<int, int>
+	> graph{}, flow{};
+	int n, p;
+	scanf("%d %d", &n, &p);
+
+	for(int i = 0; i < p; i ++){
+		int u, v;
+		scanf("%d %d", &u, &v);
+		graph[u][v] ++;
+		graph[v][u] ++;
+	}
+
+	int result = 0, s = 1, e = 2;
+	while(true){
+		int table[N] = { 0, };
+		queue<int> q{};
+		q.push(s);
+
+		while(!q.empty() && !table[e]){
+			int cur = q.front(); q.pop();
+			for(auto& [next, w]: graph[cur]){
+				if(w <= flow[cur][next]) continue;
+				if(table[next]) continue;
+
+				q.push(next);
+				table[next] = cur;
+
+				if(next == e) break;
+			}
+		}
+
+		if(!table[e]) break;
+
+		int f = graph[table[e]][e] - flow[table[e]][e];
+		for(int i = table[e]; i != s; i = table[i])
+			f = std::min(
+				f, graph[table[i]][i] - flow[table[i]][i]
+			);
+
+		for(int i = e; i != s; i = table[i]){
+			flow[table[e]][e] += f;
+			flow[e][table[e]] -= f;
+		}
+
+		result += f;
+
+	}
+	printf("%d\n", result);
+	return 0;
+}
+

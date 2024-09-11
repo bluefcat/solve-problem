e70aeea446f428cf1a574cb45e621938639d5a3d
bluefcat <bluefcat@gmail.com>
2024-09-11 13:18:35 +0900

    Add solve file in 240911

--- 7001-8000/7626.cpp
+++ 7001-8000/7626.cpp
@@ -1,55 +1,49 @@
 #include <cstdio>
 #include <vector>
-#include <unordered_map>
 #include <unordered_set>
+#include <unordered_map>
 #include <algorithm>
-#include <climits>
 #include <tuple>
 
 using ll = long long;
-using std::vector;
-using std::unordered_map;
-using std::unordered_set;
-using std::tuple;
+using set = std::unordered_set<ll>;
+using map = std::unordered_map<ll, ll>;
+using vector = std::vector<ll>;
+using query = std::vector<std::tuple<ll, ll, ll>>;
 
-constexpr int N = 200001;
+constexpr int N = 400001;
 constexpr int get_left(int idx){ return (idx << 1) + 1; }
 constexpr int get_right(int idx){ return (idx << 1) + 2; }
-
-ll arr[N] = { 0, };
 ll tree[4*N+4][2] = { 0, };
-vector<ll> hv{};
+ll height[N] = { 0, };
 
-void update(int l, int r, int s, int e, bool oper, int idx){
-	if(r < s | e < l) return;
+ll update(int l, int r, int s, int e, bool oper, int idx){
+	if(r < s || e < l) return 0;
+	
 	if(s <= l && r <= e){
 		tree[idx][1] += oper?1:-1;
 	}
 	else{
 		int m = (l + r) >> 1;
-		if(s <= m){
-			update(l, m, s, e, oper, get_left(idx));
-		}
-		if(e >= m+1){ 
-			update(m+1, r, s, e, oper, get_right(idx));
-		}
+		update(l, m, s, e, oper, get_left(idx));
+		update(m+1, r, s, e, oper, get_right(idx));
 	}
+
 	if(tree[idx][1]){
-		tree[idx][0] = hv[r+1] - hv[l];
+		tree[idx][0] = height[r+1] - height[l];
 	}
 	else{
-	if(l == r) tree[idx][0] = 0;
-	else
-		tree[idx][0] = tree[get_left(idx)][0] + tree[get_right(idx)][0];
-
+		if(l == r) return tree[idx][0] = 0;
+		return tree[idx][0] = tree[get_left(idx)][0] + tree[get_right(idx)][0];
 	}
+	return 0;	
 }
 
-unordered_set<ll> height{};
-unordered_map<ll, ll> hidx{};
+set hset{};
+map hmap{};
 
-vector<tuple<ll, ll, ll>> adder{};
-vector<tuple<ll, ll, ll>> suber{};
+query adder{};
+query suber{};
 
 int main(){
 	int n;
@@ -57,24 +51,22 @@ int main(){
 	for(int i = 0; i < n; i ++){
 		ll x0, x1, y0, y1;
 		scanf("%lld %lld %lld %lld", &x0, &x1, &y0, &y1);
-		height.insert(y0);
-		height.insert(y1);
+		hset.insert(y0); hset.insert(y1);
 		adder.push_back(std::make_tuple(x0, y0, y1));
 		suber.push_back(std::make_tuple(x1, y0, y1));
-	}	
-	vector<ll> tmp{height.begin(), height.end()};
+	}
+	vector tmp{hset.begin(), hset.end()};
 	std::sort(tmp.begin(), tmp.end());
 
 	for(int i = 0; i < tmp.size(); i ++){
-		hidx[tmp[i]] = i;
-		if(i < tmp.size()-1) arr[i] = tmp[i+1] - tmp[i];
+		hmap[tmp[i]] = i;
+		height[i] = tmp[i];
 	}
-	hv.assign(tmp.begin(), tmp.end());
 
 	std::sort(
 		adder.begin(), 
 		adder.end(), 
-		[](tuple<ll, ll, ll>& p, tuple<ll, ll, ll>& q){
+		[](auto& p, auto& q){
 			auto& [xp0, yp0, yp1] = p;
 			auto& [xq0, yq0, yq1] = q;
 			return xp0 < xq0;
@@ -84,7 +76,7 @@ int main(){
 	std::sort(
 		suber.begin(), 
 		suber.end(), 
-		[](tuple<ll, ll, ll>& p, tuple<ll, ll, ll>& q){
+		[](auto& p, auto& q){
 			auto& [xp0, yp0, yp1] = p;
 			auto& [xq0, yq0, yq1] = q;
 			return xp0 < xq0;
@@ -94,6 +86,8 @@ int main(){
 	ll result = 0;
 	ll px = 0, ph = 0;
 	int ai = 0, si = 0;
+	ll x0, y0, y1;
+
 	while(ai < adder.size() || si < suber.size()){
 		auto [x0, y0, y1] = adder[ai];
 		auto [sx0, sy0, sy1] = suber[si];
@@ -107,15 +101,16 @@ int main(){
 			ai ++;
 		}
 
-		update(0, n, hidx[y0], hidx[y1]-1, oper, 0);
+		update(0, 2*n, hmap[y0], hmap[y1]-1, oper, 0);
 		ll a = px, b = ph;
 		result += (x0 - px) * ph;
-		ll height = tree[0][0];
+		ll h = tree[0][0];
 		px = x0;
-		ph = height;
+		ph = h;
 	}
 
 
 	printf("%lld\n", result);
+
 	return 0;
 }

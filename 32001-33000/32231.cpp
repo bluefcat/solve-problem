2049813f927e867eb1e7324bc68456c153ac919c
bluefcat <bluefcat@gmail.com>
2024-09-13 14:29:35 +0900

    Add solve file in 240913

--- 32001-33000/32231.cpp
+++ 32001-33000/32231.cpp
@@ -1,25 +1,21 @@
 #include <cstdio>
 #include <cmath>
-#include <algorithm>
+#include <utility>
 
 using std::sqrt;
 
 constexpr double ep = 1e-32;
 
-double get_c1(double x1, double y1, double x2, double y2){
-	double sc = (x2*x2-2*x1*x2+x1*x1);
-	double sd = (
-		2*x2*x2*y1*y1 + 2*x2*x2*y2*y2 - 4*x1*x2*y1*y1 - 4*x1*x2*y2*y2 +
-		2*x1*x1*y1*y1 + 2*x1*x1*y2*y2 + x2*x2*x2*x2 - 4*x1*x2*x2*x2 + 
-		6*x1*x1*x2*x2 - 4*x1*x1*x1*x2 + x1*x1*x1*x1 + y1*y1*y1*y1 +
-		y2*y2*y2*y2 - 2*y1*y1*y2*y2
-	);
-	return (2*(x2-x1))/sqrt(sd);
+double get_c2(double x1, double y1, double x2, double y2){
+	return ((y2*y2+x2*x2)-(y1*y1+x1*x1))/(2*(x2-x1));
 }
 
-double f(double c1, double y){
-	double x = sqrt(1-c1*c1*y*y)/c1;
-	return x;
+double get_c1(double c2, double x1, double y1){
+	return 1/sqrt((x1-c2)*(x1-c2)+y1*y1);
+}
+
+double f(double c1, double c2, double y){
+	return sqrt(1-c1*c1*y*y)/c1+c2;
 }
 
 double inte(double c1, double y){
@@ -37,35 +33,22 @@ int main(){
 			printf("%.6lf\n", std::abs(log(y2/y1)));
 			continue;
 		}
-
+		
 		if(x1 > x2){
-			double tmp = x1;
-			x1 = x2;
-			x2 = tmp;
-			tmp = y1;
-			y1 = y2;
-			y2 = tmp;
+			std::swap(x1, x2);
+			std::swap(y1, y2);
 		}
-		x2 = x2-x1;
-		x1 = 0;
+		double c2 = get_c2(x1, y1, x2, y2);
+		double c1 = get_c1(c2, x1, y1);
 
-		double c1 = -get_c1(x1, y1, x2, y2);
-		if(y1 > y2) c1 *= -1;
-		double c2 = x2 - f(c1, y2);
-
-		double result = 0;
-		if(y1 <= y2){
-			result = inte(c1, y2) - inte(c1, y1);
+		double result = 0.0f;
+		if(x1 <= c2){
+			result = inte(c1, 1/c1) - inte(c1, y2) + inte(c1, 1/c1) - inte(c1, y1);
 		}
 		else{
-			result = inte(c1, y1) - inte(c1, y2);
-		}
-
-		if(!(x1 - ep < f(c1, y1)+c2 && f(c1, y1)+c2 < x1 + ep)){
-			if(c1 > 0)
-				result = inte(c1, 1/c1) - inte(c1, y2) + inte(c1, 1/c1) - inte(c1, y1);
-			else
-				result = inte(c1, -1/c1) - inte(c1, y2) + inte(c1, -1/c1) - inte(c1, y1);
+			double s = y1, e = y2;
+			if(s > e) std::swap(s, e);
+			result = inte(c1, e) - inte(c1, s);
 		}
 		printf("%.6lf\n", result);
 	}

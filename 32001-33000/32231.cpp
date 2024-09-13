f9e919e5f6ab9268aa977e5b101cd25350405cc4
bluefcat <bluefcat@gmail.com>
2024-09-13 13:04:45 +0900

    Add solve file in 240913

--- 32001-33000/32231.cpp
+++ 32001-33000/32231.cpp
@@ -1,9 +1,10 @@
 #include <cstdio>
 #include <cmath>
+#include <algorithm>
 
 using std::sqrt;
 
-constexpr double ep = 1e-10;
+constexpr double ep = 1e-11;
 
 double get_c1(double x1, double y1, double x2, double y2){
 	double sc = (x2*x2-2*x1*x2+x1*x1);
@@ -33,9 +34,19 @@ int main(){
 		double x1, y1, x2, y2;
 		scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
 		if(x1 == x2){
-			printf("%lf\n", log(y2/y1));
+			printf("%.6lf\n", std::abs(log(y2/y1)));
 			continue;
 		}
+
+		if(x1 > x2){
+			double tmp = x1;
+			x1 = x2;
+			x2 = tmp;
+			tmp = y1;
+			y1 = y2;
+			y2 = tmp;
+		}
+
 		double c1 = -get_c1(x1, y1, x2, y2);
 		if(y1 > y2) c1 *= -1;
 		double c2 = x2 - f(c1, y2);
@@ -46,10 +57,11 @@ int main(){
 		else{
 			result = inte(c1, y1) - inte(c1, y2);
 		}
+
 		if(!(x1 - ep < f(c1, y1)+c2 && f(c1, y1)+c2 < x1 + ep)){
 			result = inte(c1, 1/c1) - inte(c1, y2) + inte(c1, 1/c1) - inte(c1, y1);
 		}
-		printf("%lf\n", result);
+		printf("%.6lf\n", std::abs(result));
 	}
 	return 0;
 }

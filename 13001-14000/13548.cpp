2dde9090003b316afb4f5371368b1502f7accee8
bluefcat <bluefcat@gmail.com>
2024-09-19 16:13:55 +0900

    Add solve file in 240919

--- 13001-14000/13548.cpp
+++ 13001-14000/13548.cpp
@@ -9,11 +9,11 @@ using std::vector;
 
 constexpr int N = 100001;
 constexpr int M = 100001;
-constexpr int SN = 550;
 
 int arr[N] = { 0, };
 vector<std::pair<pair, int>> query;
 int result[M] = { 0, };
+int vnt[N] = { 0, };
 
 int main(){
 	int n;
@@ -43,31 +43,59 @@ int main(){
 		}
 	);
 	
-	int ps = -1, pe = -1;
-	int rn = -1;
+	int ps = query[0].first.first, pe = query[0].first.second;
+	int rn = arr[ps];
 	int cnt[N] = { 0, };
-	for(auto& [p, idx]: query){
+	cnt[rn] = 1;
+	vnt[cnt[rn]] = 1;
+
+	for(int i = ps+1; i <= pe; i ++){
+		int cur = arr[i];
+		vnt[cnt[cur]] --;
+		cnt[cur] ++;
+		vnt[cnt[cur]] ++;
+		if(rn < cnt[cur]) rn = cnt[cur];
+	}
+	result[query[0].second] = rn;
+
+	for(int i = 1; i < query.size(); i ++){
+		auto& [p, idx] = query[i];
 		auto& [s, e] = p;
 		while(ps < s){
-			if(ps != -1) cnt[arr[ps]] --;
+			int cur = arr[ps];
+			
+			vnt[cnt[cur]] --;
+			if(vnt[cnt[cur]] == 0 && rn == cnt[cur]) rn --; 
+			cnt[cur] --;
+			vnt[cnt[cur]]++;
 			ps ++;
 		}
 		while(e < pe){
-			cnt[arr[pe]] --;
+			int cur = arr[pe];
+
+			vnt[cnt[cur]] --;
+			if(vnt[cnt[cur]] == 0 && rn == cnt[cur]) rn --;
+			cnt[cur] --;
+			vnt[cnt[cur]] ++;
 			pe --;
 		}
 		while(pe < e){
 			pe ++;
-			cnt[arr[pe]] ++;
-			if(cnt[rn] < cnt[arr[pe]]) rn = arr[pe];
+			int cur = arr[pe];
+			vnt[cnt[cur]] --;
+			cnt[cur] ++;
+			vnt[cnt[cur]] ++;
+			if(rn < cnt[cur]) rn = cnt[cur];
 		}	
 		while(s < ps){
 			ps --;
-			cnt[arr[ps]] ++;
-			if(cnt[rn] < cnt[arr[ps]]) rn = arr[ps];
+			int cur = arr[ps];
+			vnt[cnt[cur]] --;
+			cnt[cur] ++;
+			vnt[cnt[cur]] ++;
+			if(rn < cnt[cur]) rn = cnt[cur];
 		}
-		result[idx] = cnt[rn];
-		ps = s, pe = e;
+		result[idx] = rn;
 	}
 
 

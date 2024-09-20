f6c488316f42197c0558d8f35c4e5802795cceb2
bluefcat <bluefcat@gmail.com>
2024-09-20 09:14:52 +0900

    Add solve file in 240920

--- 2001-3000/2912.cpp
+++ 2001-3000/2912.cpp
@@ -9,13 +9,12 @@ using pair = std::pair<int, int>;
 using std::vector;
 
 constexpr int N = 300001;
-constexpr int C = 10001;
 constexpr int M = 10001;
 
 int arr[N] = { 0, };
-vector<std::pair<pair, int>> query{};
-std::unordered_set<int> vcnt[N]{};
+vector<std::pair<pair, int>> query;
 int result[M][2] = { 0, };
+std::unordered_set<int> vnt[N];
 
 int main(){
 	int n, c;
@@ -44,73 +43,69 @@ int main(){
 			return pidx < qidx;
 		}
 	);
+	
 	int ps = query[0].first.first, pe = query[0].first.second;
 	int rn = 0;
-	int cnt[C] = { 0, };
+	int cnt[N] = { 0, };
 
 	for(int i = ps; i <= pe; i ++){
 		int cur = arr[i];
-	 	vcnt[cnt[cur]].erase(cur);
+		vnt[cnt[cur]].erase(cur);
 		cnt[cur] ++;
-		vcnt[cnt[cur]].insert(cur);
-		
+		vnt[cnt[cur]].insert(cur);
 		if(cnt[rn] < cnt[cur]) rn = cur;
 	}
 	result[query[0].second][0] = rn;
-	result[query[0].second][1] = (pe-ps+1) < 2*cnt[rn];
-	
+	result[query[0].second][1] = 2*cnt[rn] > (pe-ps+1);
+
 	for(int i = 1; i < query.size(); i ++){
 		auto& [p, idx] = query[i];
 		auto& [s, e] = p;
-		while(s < ps){
-			ps --;
-			int cur = arr[ps];
-
-			vcnt[cnt[cur]].erase(cur);
-			cnt[cur] ++;
-			vcnt[cnt[cur]].insert(cur);
-
-			if(cnt[rn] < cnt[arr[ps]]) rn = cur;
-		}
 		while(ps < s){
 			int cur = arr[ps];
-
-			vcnt[cnt[cur]].erase(cur);
-			int nxt = !vcnt[cnt[cur]].empty()?(*vcnt[cnt[cur]].begin()):cur;
-			cnt[cur] --;
-			vcnt[cnt[cur]].insert(cur);
 			
-			rn = nxt;
+			vnt[cnt[cur]].erase(cur);
+			if(!vnt[cnt[cur]].empty()){ 
+				rn = (*vnt[cnt[cur]].begin());
+			}
+			cnt[cur] --;
+			vnt[cnt[cur]].insert(cur);
 			ps ++;
-		}
-		while(pe < e){
-			pe ++;
-			int cur = arr[pe];
-
-			vcnt[cnt[cur]].erase(cur);
-			cnt[cur] ++;
-			vcnt[cnt[cur]].insert(cur);
-
-			if(cnt[rn] < cnt[arr[pe]]) rn = cur;
 		}
 		while(e < pe){
 			int cur = arr[pe];
 
-			vcnt[cnt[cur]].erase(cur);
-			int nxt = !vcnt[cnt[cur]].empty()?(*vcnt[cnt[cur]].begin()):cur;
+			vnt[cnt[cur]].erase(cur);
+			if(!vnt[cnt[cur]].empty()){ 
+				rn = (*vnt[cnt[cur]].begin());
+			}
 			cnt[cur] --;
-			vcnt[cnt[cur]].insert(cur);
-			
-			rn = nxt;
+			vnt[cnt[cur]].insert(cur);
 			pe --;
 		}
+		while(pe < e){
+			pe ++;
+			int cur = arr[pe];
+			vnt[cnt[cur]].erase(cur);
+			cnt[cur] ++;
+			vnt[cnt[cur]].insert(cur);
+			if(cnt[rn] < cnt[cur]) rn = cur;
+		}	
+		while(s < ps){
+			ps --;
+			int cur = arr[ps];
+			vnt[cnt[cur]].erase(cur);
+			cnt[cur] ++;
+			vnt[cnt[cur]].insert(cur);
+			if(cnt[rn] < cnt[cur]) rn = cur;
+		}
 		result[idx][0] = rn;
-		result[idx][1] = cnt[rn]*2 > (e-s+1);
+		result[idx][1] = 2*cnt[rn] > (e-s+1);
 	}
 
 
 	for(int i = 0; i < m; i ++){
-		if(result[i][1])
+		if(result[i][1])	
 			printf("yes %d\n", result[i][0]);
 		else
 			printf("no\n");

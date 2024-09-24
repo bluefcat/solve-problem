124385433c8d13b5581e0d25127457e3d04328c3
bluefcat <bluefcat@gmail.com>
2024-09-24 09:33:33 +0900

    Add solve file in 2912

--- 2001-3000/2912.cpp
+++ 2001-3000/2912.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <unordered_set>
 
 using pair = std::pair<int, int>;
 using std::vector;
@@ -13,8 +12,7 @@ constexpr int M = 10001;
 
 int arr[N] = { 0, };
 vector<std::pair<pair, int>> query;
-int result[M][2] = { 0, };
-std::unordered_set<int> vnt[N];
+int result[M] = { 0, };
 
 int main(){
 	int n, c;
@@ -45,73 +43,52 @@ int main(){
 	);
 	
 	int ps = query[0].first.first, pe = query[0].first.second;
-	int rn = 0;
 	int cnt[N] = { 0, };
 
 	for(int i = ps; i <= pe; i ++){
 		int cur = arr[i];
-		if(auto iter = vnt[cnt[cur]].find(cur); iter != vnt[cnt[cur]].end())
-			vnt[cnt[cur]].erase(cur);
 		cnt[cur] ++;
-		vnt[cnt[cur]].insert(cur);
-		if(cnt[rn] < cnt[cur]) rn = cur;
 	}
-	result[query[0].second][0] = rn;
-	result[query[0].second][1] = 2*cnt[rn] > (pe-ps+1);
+	for(int i = 0; i < M; i ++){
+		if(cnt[i] * 2 <= (pe-ps+1)) continue;
+		result[query[0].second] = i;
+		break;
+	}
 
 	for(int i = 1; i < query.size(); i ++){
 		auto& [p, idx] = query[i];
 		auto& [s, e] = p;
 		while(ps < s){
 			int cur = arr[ps];
-			
-			if(auto iter = vnt[cnt[cur]].find(cur); iter != vnt[cnt[cur]].end())
-				vnt[cnt[cur]].erase(cur);
-			if(!vnt[cnt[cur]].empty()){ 
-				rn = (*vnt[cnt[cur]].begin());
-			}
 			cnt[cur] --;
-			vnt[cnt[cur]].insert(cur);
 			ps ++;
 		}
 		while(e < pe){
 			int cur = arr[pe];
-
-			if(auto iter = vnt[cnt[cur]].find(cur); iter != vnt[cnt[cur]].end())
-				vnt[cnt[cur]].erase(cur);
-			if(!vnt[cnt[cur]].empty()){ 
-				rn = (*vnt[cnt[cur]].begin());
-			}
 			cnt[cur] --;
-			vnt[cnt[cur]].insert(cur);
 			pe --;
 		}
 		while(pe < e){
 			pe ++;
 			int cur = arr[pe];
-			if(auto iter = vnt[cnt[cur]].find(cur); iter != vnt[cnt[cur]].end())
-				vnt[cnt[cur]].erase(cur);
 			cnt[cur] ++;
-			vnt[cnt[cur]].insert(cur);
-			if(cnt[rn] < cnt[cur]) rn = cur;
 		}	
 		while(s < ps){
 			ps --;
 			int cur = arr[ps];
-			if(auto iter = vnt[cnt[cur]].find(cur); iter != vnt[cnt[cur]].end())
-				vnt[cnt[cur]].erase(cur);
 			cnt[cur] ++;
-			vnt[cnt[cur]].insert(cur);
-			if(cnt[rn] < cnt[cur]) rn = cur;
 		}
-		result[idx][0] = rn;
-		result[idx][1] = 2*cnt[rn] > (e-s+1);
+		for(int c = 0; c < M; c ++){
+			if(cnt[c]*2 <= (e-s+1)) continue;
+			result[idx] = c;
+			break;
+		}
 	}
 
 
 	for(int i = 0; i < m; i ++){
-		if(result[i][1])	
-			printf("yes %d\n", result[i][0]);
+		if(result[i])	
+			printf("yes %d\n", result[i]);
 		else
 			printf("no\n");
 	}

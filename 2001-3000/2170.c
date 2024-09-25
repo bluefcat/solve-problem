a1e82cdf26e1d2ea9511947d3fd002a61d681a6e
bluefcat <bluefcat@gmail.com>
2024-09-25 09:15:40 +0900

    Add solve file in 240925

--- 2001-3000/2170.c
+++ 2001-3000/2170.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX(X, Y) (((X) > (Y))?(X): (Y))
+
 typedef struct line_{
 	int x;
 	int y;
@@ -7,39 +9,9 @@ typedef struct line_{
 
 line cache[1000001];
 
-void swap(int x, int y){
-	line tmp = (line){ cache[x].x, cache[x].y };
-	
-	cache[x].x = cache[y].x;
-	cache[x].y = cache[y].y;
-
-	cache[y].x = tmp.x;
-	cache[y].y = tmp.y;
+int compare(const void* p, const void* q){
+	return ((const line*)p)->x > ((const line*)q)->x;
 }
-
-int partition(int l, int r){
-	int pidx = r-1;
-	int pivot = cache[pidx].x;
-	
-	int c = l;
-	for(int i = l; i < pidx; i ++){
-		if(pivot > cache[i].x){
-			swap(i, c);
-			c++;
-		}
-	}
-	
-	swap(c, pidx);
-	return c;
-}
-
-void quicksort(int l, int r){
-	if(l+1 >= r) return;
-	int pidx = partition(l, r);
-	quicksort(l, pidx);
-	quicksort(pidx+1, r);
-}
-
 
 int main(){
 	int n;
@@ -50,7 +22,7 @@ int main(){
 		cache[i].x = x; cache[i].y = y;
 	}
 	
-	quicksort(0, n);
+	qsort(cache, n, sizeof(line), compare);
 	
 	int l = 0, r = 0, result = 0;
 	for(int i = 0; i < n; i ++){

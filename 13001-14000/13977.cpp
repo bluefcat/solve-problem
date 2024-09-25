68ec8664d35a0003733af70a9a565f7597a50326
bluefcat <bluefcat@gmail.com>
2024-09-25 22:20:51 +0900

    Add solve file in 240925

--- 13001-14000/13977.cpp
+++ 13001-14000/13977.cpp
@@ -1,16 +1,10 @@
 #include <cstdio>
 
 using lint = long long;
+constexpr int N = 4000001;
 constexpr int MOD = 1000000007;
 
-lint fact(lint x){
-	lint result = 1;
-	while(x){
-		result = (result * x) % MOD;
-		x --;
-	}
-	return result;
-}
+lint cache[N] = { 0, };
 
 lint mpow(lint x, lint e){
 	lint result = 1;
@@ -25,13 +19,17 @@ lint mpow(lint x, lint e){
 int main(){
 	int m;
 	scanf("%d", &m);
+	cache[0] = 1; cache[1] = 1;
+	for(int i = 2; i < N; i ++){
+		cache[i] = (i * cache[i-1]) % MOD;
+	}
 
 	for(int i = 0; i < m; i ++){
 		lint n, k;
 		scanf("%lld %lld", &n, &k);
-		lint rk = mpow(fact(k), MOD-2);
-		lint rnk = mpow(fact(n-k), MOD-2);
-		printf("%lld\n", ((fact(n)*rk)%MOD)*rnk%MOD);	
+		lint rk = mpow(cache[k], MOD-2);
+		lint rnk = mpow(cache[n-k], MOD-2);
+		printf("%lld\n", ((cache[n]*rk%MOD)*rnk)%MOD);	
 
 	}
 	return 0;

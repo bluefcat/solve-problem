54c1ad0a8f98192dec0db1cc76361937244357c6
bluefcat <bluefcat@gmail.com>
2024-09-26 09:25:54 +0900

    Add solve file in 240926

--- 5001-6000/5430.py
+++ 5001-6000/5430.py
@@ -1,25 +1,34 @@
+import sys
 from collections import deque
 
-T = int(input())
+input = sys.stdin.readline
+
+T = int(input().strip())
 
 for _ in range(T):
-    P = input()
-    n = int(input())
-    content = input()[1:-1]
+    P = input().strip()
+    n = int(input().strip())
+    content = input().strip()[1:-1]
+    r = False
     if not content:
         arr = deque([])
     else:
         arr = deque(map(int, (content).split(',')))
     for oper in P:
         if oper == 'R':
-            arr.reverse()
+            r ^= True
         elif oper == 'D':
             if not arr:
                 print("error")
                 break
             else:
-                arr.popleft()
+                if r:
+                    arr.pop()
+                else:
+                    arr.popleft()
     else:
+        if r:
+            arr.reverse()
         result = ",".join(map(str, arr))
         print(f"[{result}]")
 

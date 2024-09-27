c4ef620a8ba25087ad16130d2b3c81871bc91d45
bluefcat <bluefcat@gmail.com>
2024-09-27 11:15:21 +0900

    Add solve file in 240927

--- 1001-2000/1311.cpp
+++ 1001-2000/1311.cpp
@@ -3,44 +3,32 @@
 #include <unordered_map>
 
 using std::min;
-using std::unordered_map;
 
 constexpr int N = 20;
-unordered_map<int, int> cache[N+1]{};
+int cache[1 << N] = { 0, };
 
 int main(){
 	int field[N+1][N] = { 0, };
 	int n;
 	scanf("%d", &n);
-	cache[0][0] = 0;
 	for(int i = 1; i <= n; i ++){
 		for(int j = 0; j < n; j ++){
 			scanf("%d", field[i]+j);
 		}
 	}
 	
-	//employee
-	for(int i = 1; i <= n; i ++){
-		for(auto [flag, prev]: cache[i-1]){
-			for(int t = 0; t < n; t ++){
-				int task = (1 << t);
-				if((flag & task) == task) continue;
-				if(cache[i][flag | task] == 0)
-					cache[i][flag | task] = prev + field[i][t];
-				cache[i][flag | task] = min(
-					cache[i][flag | task],
-					prev + field[i][t]
-				);
-			}
-
+	for(int s = 0; s < (1 << n); s ++){
+		int i = __builtin_popcount(s) + 1;
+		for(int t = 0; t < n; t ++){
+			int task = 1 << t;
+			if((s & task) == task) continue;
+			if(cache[s | task] != 0 && cache[s | task] < field[i][t] + cache[s])
+				continue;
+			cache[s | task] = field[i][t] + cache[s];
 		}
 	}
 
-	int result = 0;
-	for(auto [flag, prev]: cache[n]){
-		result = prev;
-	}
-
+	int result = cache[(1 << n)-1];
 	printf("%d\n", result);
 
 	return 0;

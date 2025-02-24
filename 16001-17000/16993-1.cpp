6c0c5d0ca5b30172f682953e34cb6e711bd6471f
bluefcat <bluefcat@gmail.com>
2025-02-24 14:50:57 +0900

    Add solve file in 250224

--- 16001-17000/16993-1.cpp
+++ 16001-17000/16993-1.cpp
@@ -1,8 +1,10 @@
 #include <cstdio>
-#include <algorithm>
 #include <climits>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
 
-constexpr int N = 4*100000+4;
+constexpr int N = 100004;
 constexpr int left(int x){
 	return (x << 1) + 1;
 }
@@ -11,17 +13,31 @@ constexpr int right(int x){
 }
 
 struct Node{
-	int l, n, r;
+	int m = 0, l = 0, r = 0;
 };
 
 int arr[N];
-Node tree[N];
+int add[N];
+Node tree[4*N+4];
+
+Node combine(int l, int r, const Node* ln, const Node* rn){
+	Node result{};
+	int al = (l == 0)?0:add[l-1];
+	result.l = std::max(
+		ln->l, rn->l + (add[(l+r)>>1] - al)
+	);
+	result.r = std::max(
+		rn->r, ln->r + (add[r] - add[(l+r)>>1])
+	);
+	result.m = std::max(
+		{ln->m, rn->m, ln->r + rn->l}
+	);
+	return result;
+}
 
 Node* init(int l, int r, int idx){
 	if(l == r){
-		tree[idx] = {
-			0, arr[l], 0
-		};
+		tree[idx] = {arr[l], arr[l], arr[l]};
 		return tree + idx;
 	}
 
@@ -29,83 +45,81 @@ Node* init(int l, int r, int idx){
 	Node* lp = init(l, m, left(idx));
 	Node* rp = init(m+1, r, right(idx));
 
-	int mid = lp->n + lp->r + rp->l + rp->n;
-
-	if(mid >= lp->n && mid >= rp->n){
-		tree[idx] = {
-			lp->l, mid, rp->r
-		};
-	}
-
-	else if(lp->n > rp->n){
-		tree[idx] = {
-			lp->l, 
-			lp->n, 
-			lp->r + (rp->l + rp->n + rp->r)
-		};
-	}
-	else{
-		tree[idx] = {
-			(lp->l + lp->n + lp->r) + rp->l,
-			rp->n,
-			rp->r
-		};
-	}
-
-
+	tree[idx] = combine(l, r, lp, rp);
 	return tree + idx;
 }
 
 Node query(int l, int r, int s, int e, int idx){
-	if(r < s || e < l) return {0, INT_MIN, 0};
+	if(r < s || e < l) return {INT_MIN, INT_MIN, INT_MIN};
 	if(s <= l && r <= e) return tree[idx];	
+
 	int m = (l + r) >> 1;
 	Node lp = query(l, m, s, e, left(idx));
 	Node rp = query(m+1, r, s, e, right(idx));
 	
-	if(lp.n == INT_MIN) return {rp.l, rp.n, rp.r};
-	if(rp.n == INT_MIN) return {lp.l, lp.n, lp.r};
+	if(lp.m == INT_MIN) return rp;
+	if(rp.m == INT_MIN) return lp;
 
-
-	int mid = lp.n + lp.r + rp.l + rp.n;
-	
-	if(mid >= lp.n && mid >= rp.n){
-		return {
-			lp.l, mid, rp.r
-		};
-	}
-
-	else if(lp.n > rp.n){
-		return {
-			lp.l, 
-			lp.n, 
-			lp.r + (rp.l + rp.n + rp.r)
-		};
-	}
-	else{
-		return {
-			(lp.l + lp.n + lp.r) + rp.l,
-			rp.n,
-			rp.r
-		};
-	}
-
+	return combine(l, r, &lp, &rp);
 }
 
+int get_answer(int l, int r){
+	int cache[N] = { 0, };
+	int result = INT_MIN;
+	cache[0] = arr[l];
+	for(int i = 1; i <= r-l; i ++){
+		cache[i] = std::max(
+			cache[i-1] + arr[l+i], arr[l+i]
+		);
+	}
+	for(int i = 0; i <= r-l; i ++){
+		result = std::max(result, cache[i]);
+	}
+	return result;
+	return 1;
+}
 
 int main(){
+	srand((unsigned int)time(NULL));
 	int n, m;
-	scanf("%d", &n);
-	for(int i = 0; i < n; i ++)
-		scanf("%d", arr+i);
+	n = 1024;
+	//scanf("%d", &n);
+	int count = 0;
+	while(true){
+		count ++;
+		printf("attempt: [%d]\n", count);
+		for(int i = 0; i < n; i ++){
+			arr[i] = (rand() % 21) - 10;
+			//scanf("%d", arr+i);
 
-	init(0, n-1, 0);
-	
+			add[i] = arr[i];
+			if(i != 0) add[i] += add[i-1]; // add[i] = add[i-1] + arr[i];
+		}
+		init(0, n-1, 0);
+
+		for(int s = 1; s <= n; s ++){
+			for(int e = s; e <= n; e ++){
+				Node node = query(0, n-1, s-1, e-1, 0);
+				int answer = node.m;
+				bool a = (answer == get_answer(s-1, e-1));
+				if(!a){
+					for(int i = 0; i < n; i ++) printf("%d ", arr[i]); printf("\n");
+					printf("query: (%d %d)", s, e);
+					printf("%d != %d\n", answer, get_answer(s-1, e-1));
+
+					printf("\n");
+					return 0;
+				}
+			}
+		}
+	}
+
+
 	scanf("%d", &m);
 	for(int i = 0; i < m; i ++){
 		int s, e;
 		scanf("%d %d", &s, &e);
-		printf("%d\n", query(0, n-1, s-1, e-1, 0).n);
+		printf("%d\n", query(0, n-1, s-1, e-1, 0).m);
 	}
 
 	return 0;
--- 16001-17000/16993.cpp
+++ 16001-17000/16993.cpp
@@ -1,82 +1,100 @@
 #include <cstdio>
-#include <algorithm>
 #include <climits>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
 
-constexpr int N = 100001;
-constexpr int get_left(int x){return (x << 1) + 1;}
-constexpr int get_right(int x){return (x << 1) + 2;}
-
-struct Node{
-	int n, sum, l, r;
-};
-
-Node& init(int arr[N], Node tree[N], int l, int r, int idx){
-	if(l == r){
-		tree[idx] = {arr[l], arr[l], l, r};
-		return tree[idx];
-	}
-	
-	int m = (l + r) >> 1;
-	Node& ln = init(arr, tree, l, m, get_left(idx));
-	Node& rn = init(arr, tree, m+1, r, get_right(idx));
-
-	int sum = ln.sum+rn.sum;
-	int mid = INT_MIN;
-
-	if(ln.r + 1 == rn.l) mid = ln.n + rn.n;
-	if(mid >= std::max(ln.n, rn.n) || sum >= std::max(ln.n, rn.n))
-		tree[idx] = {std::max(mid, sum), sum, ln.l, rn.r};
-	else{
-		if(ln.n > rn.n)
-			tree[idx] = {ln.n, sum, ln.l, ln.r};
-		else
-			tree[idx] = {rn.n, sum, rn.l, rn.r};
-	}
-	return tree[idx];
+constexpr int N = 100004;
+constexpr int left(int x){
+	return (x << 1) + 1;
+}
+constexpr int right(int x){
+	return (x << 1) + 2;
 }
 
-Node query(Node tree[N], int l, int r, int s, int e, int idx){
-	if(r < s || e < l) return Node{INT_MIN, 0, -1, -1};
-	if(s <= l && r <= e) return tree[idx];
+struct Node{
+	int m = 0, l = 0, r = 0;
+};
+
+int arr[N];
+int add[N];
+Node tree[4*N+4];
+
+Node combine(int l, int r, const Node* ln, const Node* rn){
+	Node result{};
+	int al = (l == 0)?0:add[l-1];
+	result.l = std::max(
+		ln->l, rn->l + (add[(l+r)>>1] - al)
+	);
+	result.r = std::max(
+		rn->r, ln->r + (add[r] - add[(l+r)>>1])
+	);
+	result.m = std::max(
+		{ln->m, rn->m, ln->r + rn->l}
+	);
+	return result;
+}
+
+Node* init(int l, int r, int idx){
+	if(l == r){
+		tree[idx] = {arr[l], arr[l], arr[l]};
+		return tree + idx;
+	}
 
 	int m = (l + r) >> 1;
-	Node result{};
-	Node ln = query(tree, l, m, s, e, get_left(idx));
-	Node rn = query(tree, m+1, r, s, e, get_right(idx));
+	Node* lp = init(l, m, left(idx));
+	Node* rp = init(m+1, r, right(idx));
 
-	int sum = ln.sum+rn.sum;
-	int mid = INT_MIN;
+	tree[idx] = combine(l, r, lp, rp);
+	return tree + idx;
+}
+
+Node query(int l, int r, int s, int e, int idx){
+	if(r < s || e < l) return {INT_MIN, INT_MIN, INT_MIN};
+	if(s <= l && r <= e) return tree[idx];	
+
+	int m = (l + r) >> 1;
+	Node lp = query(l, m, s, e, left(idx));
+	Node rp = query(m+1, r, s, e, right(idx));
 	
-	if(ln.l != -1 && rn.r != -1){
-		if(ln.r + 1 == rn.l) mid = ln.n + rn.n;
-		if(mid >= std::max(ln.n, rn.n) || sum >= std::max(ln.n, rn.n))
-			return {std::max(mid, sum), sum, ln.l, rn.r};
+	if(lp.m == INT_MIN) return rp;
+	if(rp.m == INT_MIN) return lp;
+
+	return combine(l, r, &lp, &rp);
+}
+
+int get_answer(int l, int r){
+	int cache[N] = { 0, };
+	int result = INT_MIN;
+	cache[0] = arr[l];
+	for(int i = 1; i <= r-l; i ++){
+		cache[i] = std::max(
+			cache[i-1] + arr[l+i], arr[l+i]
+		);
 	}
-	
-	if(ln.n > rn.n)
-		result = {ln.n, sum, ln.l, ln.r};
-	else
-		result = {rn.n, sum, rn.l, rn.r};
-	
+	for(int i = 0; i <= r-l; i ++){
+		result = std::max(result, cache[i]);
+	}
 	return result;
-
+	return 1;
 }
 
 int main(){
-	int arr[N] = { 0, };
-	Node tree[4*N+4] = { 0, };
-	
+	srand((unsigned int)time(NULL));
 	int n, m;
 	scanf("%d", &n);
-	for(int i = 0; i < n; i ++)
+	for(int i = 0; i < n; i ++){
 		scanf("%d", arr+i);
-	init(arr, tree, 0, n-1, 0);
-	
+		add[i] = arr[i];
+		if(i != 0) add[i] += add[i-1]; // add[i] = add[i-1] + arr[i];
+	}
+	init(0, n-1, 0);
+
 	scanf("%d", &m);
 	for(int i = 0; i < m; i ++){
 		int s, e;
 		scanf("%d %d", &s, &e);
-		printf("%d\n", query(tree, 0, n-1, s-1, e-1, 0).n);
+		printf("%d\n", query(0, n-1, s-1, e-1, 0).m);
 	}
 
 	return 0;

50feaab7264208b8167bbc1ecbe9e1f42fe101d5
bluefcat <bluefcat@gmail.com>
2025-04-11 10:07:44 +0900

    Add solve file in 250411

---
+++ 3001-4000/3079.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+using lint = long long;
+constexpr int N = 100'001;
+lint arr[N] = { 0, };
+
+bool check(lint n, lint m, lint t){
+	if(t == 0) return false;
+	
+	lint total = 0;
+	for(int i = 0; i < n; i ++){
+		total += t / arr[i];
+	}
+	return total >= m;
+}
+
+int main(){
+	lint n, m;
+	scanf("%lld %lld", &n, &m);
+	lint k = 0;
+	for(int i = 0; i < n; i ++){
+		scanf("%lld", arr+i);
+		if(k < arr[i]) k = arr[i];	
+	}
+	
+	lint no = 0, yes = m * k;
+
+	while(no+1 < yes){
+		lint mid = (no + yes) >> 1;
+		if(check(n, m, mid)) yes = mid;
+		else no = mid;
+	}
+
+	printf("%lld\n", yes);
+	return 0;
+}

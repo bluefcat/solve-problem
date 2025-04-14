9c67c3f59ae2a4fb3422c1456d05ca50e6984262
bluefcat <bluefcat@gmail.com>
2025-04-14 13:41:21 +0900

    Add solve file in 250414

--- 1001-2000/1477.cpp
+++ 1001-2000/1477.cpp
@@ -6,18 +6,18 @@ constexpr int N = 52;
 lint arr[N];
 
 bool check(lint n, lint m, lint d){
-	int total = 0;
+	lint total = 0;
 	for(int i = 0; i < n; i ++)
-		total += (arr[i] / d) - (arr[i] % d == 0);
+		total += ((arr[i] - 1) / d);
 	
-	return !(total > m);
+	return total <= m;
 }
 
 int main(){
 	lint n, m, l;
 	scanf("%lld %lld %lld", &n, &m, &l);
 	
-	arr[0] = 0, arr[n+1] = l-1;
+	arr[0] = 0, arr[n+1] = l;
 	for(int i = 1; i <= n; i ++)
 		scanf("%lld", arr+i);
 	std::sort(arr, arr+n+2);
@@ -27,7 +27,7 @@ int main(){
 	
 	std::sort(arr, arr+n+1, [](lint x, lint y){ return x > y; });
 
-	lint no = 0, yes = l;
+	lint no = 1, yes = l;
 	
 	while(no + 1 < yes){
 		lint mid = (yes + no) >> 1;

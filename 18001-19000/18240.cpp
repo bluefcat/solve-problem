483bfad56a5991194914ca5ae04ded356864f433
bluefcat <bluefcat@gmail.com>
2025-04-23 16:22:57 +0900

    Add solve file in 250422

--- 18001-19000/18240.cpp
+++ 18001-19000/18240.cpp
@@ -1,36 +1,17 @@
 #include <cstdio>
+#include <queue>
 
 struct Node{
 	int id = -1;
-	int x = -1;
+	int depth = 0;
 	Node *left{nullptr}, *right{nullptr};
 };
 
 int arr[300002];
+int size = 0;
 
 Node tree[300002];
-int count = 0;
-bool flag = false;
 
-void set_node(Node* head, int depth){
-	if(flag) return;
-	if(depth == 0){
-		if(head->left == nullptr)
-			head->left = tree + count+1;
-		else if(head->right == nullptr)
-			head->right = tree + count+1;
-		else return;
-		count ++;
-		tree[count].id = count;
-		flag = true;
-		return;
-	}
-	if(head->left) 
-		set_node(head->left, depth - 1);
-
-	if(head->right)
-		set_node(head->right, depth - 1);
-}
 
 void predict_node(Node* head, int& count){
 	if(head->left)
@@ -44,22 +25,57 @@ void predict_node(Node* head, int& count){
 
 int main(){
 	tree[0].id = 0; //head;
+	size++;
 
-	int n, has_answer = 0;
+	int n;
 	scanf("%d", &n);
+	std::queue<Node*> q({tree+0});
+
+
 	for(int i = 1; i < n; i ++){
 		int depth;
 		scanf("%d", &depth);
-		flag = false;
-		set_node(tree+0, depth-1);
-		if(flag == false) has_answer = -1;
-	}
-	if(has_answer == -1){
-		printf("-1\n");
-		return 0;
+		std::queue<Node*> tmp{};
+		int de = -1;
+		while(!q.empty() && de != depth){
+			Node* cur = q.front(); q.pop();
+			if(cur->left == nullptr && cur->depth + 1 == depth){
+				cur->left = tree+size;
+				tree[size].id = size;
+				tree[size].depth = cur->depth + 1;
+				de = tree[size].depth;
+				size ++;
+
+				tmp.push(cur->left);
+			}
+			else if(cur->right ==nullptr && cur->depth+1 ==depth){
+				cur->right = tree+size;
+				tree[size].id = size;
+				tree[size].depth = cur->depth + 1;
+				de = tree[size].depth;
+				size ++;
+				tmp.push(cur->right);
+			}
+
+			if(!(cur->left && cur->right))
+				tmp.push(cur);
+		}
+
+		while(!tmp.empty()){
+			q.push(tmp.front()); tmp.pop();
+		}
 	}
 	int count = 1;
 	predict_node(tree+0, count);
+	
+	for(int i = 0; i < n; i ++){
+		if(arr[i] == 0){
+			printf("-1\n");
+			return 0;
+		}
+	}
+
+
 	for(int i = 0; i < n; i ++){
 		printf("%d ", arr[i]);
 	}

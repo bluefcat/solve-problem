881d34c7c0a28c6c8a6c67e92dc584d65fe2271f
bluefcat <bluefcat@gmail.com>
2025-04-29 11:25:59 +0900

    Add solve file in 250429

--- 25001-26000/25797.cpp
+++ 25001-26000/25797.cpp
@@ -1,35 +1,110 @@
 #include <cstdio>
+#include <array>
 #include <vector>
 #include <tuple>
 #include <queue>
 #include <utility>
 #include <algorithm>
 
+using std::array;
+using std::vector;
+using std::tuple;
+using cord = std::pair<int, int>;
 constexpr int N = 11;
 
 enum Direction{
-	UP = 0, DOWN, LEFT, RIGHT
+	UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3
+};
+
+vector<tuple<Direction, int, int>> mov{
+	{UP, -1, 0}, {DOWN, 1, 0}, {LEFT, 0, -1}, {RIGHT, 0, 1}
 };
 
 struct Vertex{
 	int px = 0, py = 0;
 	int color = 0;
 	int area = 0;
-	int x = -1;
+	int number = -1;
 	Vertex* next[4];
 };
 
 Vertex* field[N][N];
 char board[2*N+2][2*N+2];
 
-void rotate(std::pair<int, int>& p){
-	auto [y, x] = p;
-	p = {-x, y};
+array<vector<cord>, 2> get_blocks(
+	int area,
+	Vertex* start,
+	bool visited[][N]
+){
+	array<vector<cord>, 2> result;
+	std::queue<Vertex*> q{{start}};
+	result[start->color].push_back({start->py, start->px});
+	visited[start->py][start->px] = true;
+	start->area = area;
+	
+	while(!q.empty()){
+		Vertex* cur = q.front(); q.pop();
+		for(auto* next: cur->next){
+			if(!next) continue;
+			if(visited[next->py][next->px]) continue;
+			
+			q.push(next);
+			result[next->color].push_back({next->py, next->px});
+			visited[next->py][next->px] = true;
+			next->area = area;
+		}
+	}
+	return result;
 }
 
-void flip(std::pair<int, int>& p){
-	auto [y, x] = p;
-	p = {y, -x};
+void move_origin(vector<cord>& vec){
+	int my = N+1, mx = N+1;
+	for(auto [y, x]: vec){
+		my = std::min(y, my);
+		mx = std::min(x, mx);
+	}	
+
+	for(auto& p: vec){
+		auto [y, x] = p;
+		p = {y - my, x - mx};
+	}
+}
+
+void rotate(vector<cord>& vec){
+	for(auto& p: vec){
+		auto [y, x] = p;
+		p = {-x, y};
+	}
+}
+
+void flip(vector<cord>& vec){
+	for(auto& p: vec){
+		auto [y, x] = p;
+		p = {y, -x};
+	}
+}
+
+bool check_shape(vector<cord>& white, vector<cord>& gray){
+	move_origin(gray);
+
+	bool shape[N][N] = { false, };
+	for(auto [y, x]: gray) shape[y][x] = true;
+	for(int f = 0; f < 2; f ++){
+		for(int r = 0; r < 5; r ++){
+			move_origin(white);
+			bool checker = true;
+			for(auto [y, x]: white){
+				if(shape[y][x] == false) checker = false;				
+			}
+			if(checker) return true;
+			
+			rotate(white);
+		}
+		flip(white);
+		move_origin(white);
+	}
+
+	return false;
 }
 
 int main(){
@@ -62,17 +137,13 @@ int main(){
 	for(int i = 0; i < k; i ++){
 		int r, c, x;
 		scanf("%d %d %d", &r, &c, &x);
-		field[r-1][c-1]->x = x;
+		field[r-1][c-1]->number = x;
 	}
 
 	for(int i = 0; i < 2*n+1; i ++){
 		scanf("%s", board[i]);
 	}
 	
-	std::vector<std::tuple<Direction, int, int>> mov{
-		{UP, -1, 0}, {DOWN, 1, 0}, {LEFT, 0, -1}, {RIGHT, 0, 1}
-	};
-
 
 	for(int i = 1; i < 2*n+1; i += 2){
 		for(int j = 1; j < 2*n+1; j +=2){
@@ -83,173 +154,98 @@ int main(){
 			}
 		}
 	}
-	
-
 
 	bool visited[N][N] = { 0, };
-	bool flag = true;	
-	int cnt = -1;
+
+	vector<array<vector<cord>, 2>> bucket{};
+
+	int area = 0;
+	int result = 1;
+
 	for(int i = 0; i < n; i ++){
 		for(int j = 0; j < n; j ++){
 			if(visited[i][j]) continue;
-
-			std::queue<Vertex*> q({field[i][j]});
-			cnt ++;
-			field[i][j]->area = cnt;
-			visited[i][j] = true;
-
-			int w = 0, b = 0, p = -1;
-			Vertex* start_w = nullptr;
-			Vertex* start_b = nullptr;
-
-			std::pair<int, int> sw[2*N];
-			std::pair<int, int> sb[2*N];
-			
-			while(!q.empty()){
-				Vertex* cur = q.front(); q.pop();
-				if(cur->color){ 
-					if(!start_b) start_b = cur;
-					sb[b] = {cur->py, cur->px};
-					b++;
-					
-				}
-				else{ 
-					if(!start_w) start_w = cur;
-					sw[w] = {cur->py, cur->px};
-					w ++;
-				}
-				if(p == -1){
-					p = cur->x;
-				}
-				else if(p != -1 && cur->x != -1){
-					flag &= false;	
-				}	
-				for(auto& [dir, dx, dy]: mov){
-					Vertex* nx = cur->next[dir];
-					if(nx == nullptr) continue;	
-					if(visited[nx->py][nx->px]) continue;
-					visited[nx->py][nx->px] = true;
-					nx->area=cnt;
-					q.push(nx);
-				}
-			}
-
-
-			if(w != b){
-				flag &= false;
-				break;
-			}
-			if(p != -1 && (w != p || b != p)){
-				flag &= false;
-				break;
-			}
-			
-			int mx = 99, my = 99;
-			for(int i = 0; i < w; i ++){
-				my = std::min(sw[i].first, my);	
-				mx = std::min(sw[i].second, mx);	
-			}
-			for(int i = 0; i < w; i ++){
-				sw[i] = {sw[i].first - my, sw[i].second - mx};
-			}
-
-			
-			bool same_shape = false;
-			//check shape
-			for(int f = 0; f < 2; f++){
-				for(int r = 0; r < 5; r ++){
-					int bp[N] = { 0, };	
-					int mbx = 99, mby = 99;
-					for(int i = 0; i < b; i ++){
-						mby = std::min(sb[i].first, mby);	
-						mbx = std::min(sb[i].second, mbx);	
-					}
-					for(int i = 0; i < b; i ++)
-						sb[i] = {sb[i].first - mby, sb[i].second - mbx};
-					
-					int tcount = 0;
-					for(int i = 0; i < b; i ++){
-						bool tmp = false;
-						for(int j = 0; j < w; j ++){
-							if(sb[i].first == sw[j].first && sb[i].second == sw[j].second){
-								tmp = true;
-							}	
-						}
-						tcount += (int)tmp;
-					}
-					
-					if(tcount == b) same_shape = true;
-					for(int i = 0; i < b; i ++){
-						rotate(sb[i]);
-					}
-
-				}
-
-				for(int i = 0; i < b; i ++){
-					flip(sb[i]);
-				}
-			}
-			if(!same_shape){
-				flag &= false;
-				break;
-			}
-			
-			bool shape_visited[2][N][N] = {false};
-			int wsc = 1, bsc = 1;
-			
-			while(!q.empty()) q.pop();
-			q.push(start_w);
-			shape_visited[0][start_w->py][start_w->px] = true;
-			while(!q.empty()){
-				Vertex* cur = q.front(); q.pop();
-				for(auto& [dir, dx, dy]: mov){
-					Vertex* nx = cur->next[dir];
-					if(nx == nullptr) continue;	
-					if(shape_visited[0][nx->py][nx->px]) continue;
-					if(nx->color != 0) continue;
-					shape_visited[0][nx->py][nx->px] = true;
-					wsc ++;
-					q.push(nx);
-				}
-			}
-			while(!q.empty()) q.pop();
-			q.push(start_b);
-			shape_visited[1][start_b->py][start_b->px] = true;
-			while(!q.empty()){
-				Vertex* cur = q.front(); q.pop();
-				for(auto& [dir, dx, dy]: mov){
-					Vertex* nx = cur->next[dir];
-					if(nx == nullptr) continue;	
-					if(shape_visited[1][nx->py][nx->px]) continue;
-					if(nx->color != 1) continue;
-					shape_visited[1][nx->py][nx->px] = true;
-					bsc ++;
-					q.push(nx);
-				}
-			}
-			if(bsc != b || wsc != w){ 
-				flag &= false;
-				break;
-			}
-
+			bucket.emplace_back(get_blocks(area, field[i][j], visited));
+			area ++;
 		}
 	}
-	for(int y = 0; y < n; y ++){
-		for(int x = 0; x < n; x ++){
-			Vertex* cur = field[y][x];
-			for(auto& [dir, dy, dx]: mov){
-				if(!(0 <= x+dx && x+dx < n) || !(0 <= y+dy && y+dy <n)) continue;
-				if(
-					(cur->area == field[y+dy][x+dx]->area) &&
-					(cur->next[dir] == nullptr)
-				){
-					flag &= false;
-					break;
+
+	for(int i = 0; i < N*N; i ++) visited[i/N][i%N] = false;
+
+	for(auto& blocks: bucket){
+		auto& [white, gray] = blocks;
+		//area check
+		int number = -1;
+		for(auto& block: blocks){
+			for(auto [y, x]: block){
+				Vertex* cur = field[y][x];
+				if(number == -1 && cur->number != -1)
+					number = cur->number;
+				else if(number != -1 && cur->number != -1)
+					result --;
+			}
+		}
+
+		if(white.size() != gray.size())
+			result --;
+
+		if(number != -1 && (white.size() != number || gray.size() != number))
+			result --;
+
+		//two area check
+		if(white.size() == 0 || gray.size() == 0)
+			result --;
+
+		//split check
+		for(auto& block: blocks){
+			if(block.size() == 0) break;
+			auto [y, x] = block[0];
+			Vertex* start = field[y][x];
+			vector<Vertex*> tmp{};
+
+			std::queue<Vertex*> q{{start}};
+			visited[start->py][start->px] = true;
+			tmp.push_back(start);
+			while(!q.empty()){
+				Vertex* cur = q.front(); q.pop();
+				for(auto next: cur->next){
+					if(!next) continue;
+					if(visited[next->py][next->px]) continue;
+					if(start->color != next->color) continue;
+					q.push(next);
+					visited[next->py][next->px] = true;
+					tmp.push_back(next);
+				}
+			};
+
+			if(block.size() != tmp.size())
+				result --;
+		}
+
+
+		//wall check
+		for(auto& block: blocks){
+			for(auto [y, x]: block){
+				Vertex* cur = field[y][x];
+				for(auto [dir, dy, dx]: mov){
+					if(!(0 <= x+dx && x+dx < n)) continue;
+					if(!(0 <= y+dy && y+dy < n)) continue;
+
+					Vertex* next = field[y+dy][x+dx];
+					if(
+						next->area == cur->area && 
+						cur->next[dir] == nullptr
+					) result --;
 				}
 			}
 		}
+
+		//shape check
+		if(!check_shape(white, gray))
+			result --;
 	}
-	printf("%d\n", flag);
+
+	printf("%d\n", result != 1? 0: 1);
 
 	for(int i = 0; i < N*N; i ++)
 		delete field[i/N][i%N];

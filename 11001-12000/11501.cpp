926ff9657592b50b6ee1422d27d4ebc5d7d45337
bluefcat <bluefcat@gmail.com>
2025-05-07 15:00:53 +0900

    Add solve file in 250507

--- 11001-12000/11501.cpp
+++ 11001-12000/11501.cpp
@@ -1,41 +1,30 @@
 #include <cstdio>
 #include <vector>
+#include <cmath>
 
 using lint = long long;
 constexpr int N = 1'000'001;
+lint arr[N] = { 0, };
+lint max[N] = { 0, };
 
 int main(){
 	int t;
 	scanf("%d", &t);
 	while(t--){
-		lint arr[N] = { 0, };
-		std::vector<int> sell{};
 		int n;
 		scanf("%d", &n);
-		lint max = 0, left = -1;
 		for(int i = 0; i < n; i ++){ 
 			scanf("%lld", arr+i);
-			if(left == -1 || max < arr[i])
-				max = arr[i], left = i;
 		}
-		sell.push_back(-1);
-		sell.push_back(left);
-		while(left < n-1){
-			lint tmp = left+1;
-			max = arr[left+1];
-			for(int i = left+1; i < n; i ++){
-				if(max < arr[i])
-					max = arr[i], tmp = i;
-			}
-			if(left+1 == tmp) break;
-			sell.push_back(tmp);
-			left = tmp;
-		}
+		
+		max[n-1] = arr[n-1];
+		for(int i = n-2; i >= 0; i --)
+			max[i] = std::max(max[i+1], arr[i]);
+
+
 		lint result = 0;
-		for(int i = 0; i < sell.size()-1; i ++){
-			int l = sell[i] + 1, r = sell[i+1];	
-			for(int j = l; j < r; j ++)
-				result += arr[r] - arr[j];
+		for(int i = 0; i < n; i ++){
+			result += std::max((lint)0, max[i] - arr[i]);
 		}
 		printf("%lld\n", result);
 	}

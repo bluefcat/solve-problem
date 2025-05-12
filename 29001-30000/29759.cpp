0dd503cfdf822edf8664cee617118a5da607262e
bluefcat <bluefcat@gmail.com>
2025-05-12 13:36:01 +0900

    Add solve file in 250512

---
+++ 29001-30000/29759.cpp
@@ -0,0 +1,82 @@
+#include <cstdio>
+
+constexpr int N = 500'001;
+constexpr int M = 11;
+bool prime[N];
+bool used[2*N+3];
+
+int board[M*M][M*M];
+
+void build_sieve(){
+	for(int i = 0; i < N; i ++) prime[i] = true;
+	int idx = 3;
+	int k = 3;
+	for(int i = 0; idx < N; i ++){
+		if(prime[i]){ //2*i+3
+			prime[idx] = false;
+			for(int j = idx; N - j > k; j += k){
+				prime[j] = false;
+			}
+		}
+		idx += k;
+		k += 2;
+		idx += k;
+	}
+}
+
+int main(){
+	build_sieve();
+
+	int n;
+	scanf("%d", &n);
+
+	for(int i = 0; i < n*n; i ++){
+		for(int j = 0; j < n*n; j ++){
+			int tmp;
+			scanf("%d", &tmp);
+			board[i][j] = tmp;
+			if(tmp == 0) continue;
+			for(int pi = -1; pi < N; pi ++){
+				if(pi == -1){
+					if(tmp & 1) continue;
+					used[2] = true;
+				}
+				else if(prime[pi] && tmp % (2*pi+3) == 0)
+					used[2*pi+3] = true;
+			}
+			
+		}
+	}
+	
+	int last = 0;
+	for(; last < N; last ++){
+		if(prime[last] && !used[2*last+3]) break;
+	}
+
+	for(int i = 0; i < n*n; i ++){
+		for(int j = 0; j < n*n; j ++){
+			if(board[i][j] != 0 ) continue;
+			if(!used[2]){
+				board[i][j] = 2;
+				used[2] = true;
+				continue;
+			}
+			board[i][j] = 2*last+3;
+			used[2*last+3] = true;
+			for(; last < N; last ++){
+				if(prime[last] && !used[2*last+3]) break;
+			}
+			
+		}
+	}	
+	
+	for(int i = 0; i < n*n; i ++){
+		for(int j = 0; j < n*n; j ++){
+			printf("%d ", board[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}

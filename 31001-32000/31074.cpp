4addb8ebc5be889b4ca6bfbdacfa2d9209469b13
bluefcat <bluefcat@gmail.com>
2025-05-14 17:34:10 +0900

    Add solve file in 250514

--- 31001-32000/31074.cpp
+++ 31001-32000/31074.cpp
@@ -1,17 +1,14 @@
 #include <cstdio>
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <vector>
 #include <algorithm>
 #include <utility>
-constexpr int N = 1'002;
 
-int arr[N];
 
 std::vector<int> count(int l, int r){
 	printf("count %d %d\n", l, r);
-	std::cout << std::flush;
+	fflush(stdout);
 	int x, y, z, w;
 	scanf("%d %d %d %d", &x, &y, &z, &w);
 	std::vector count{x, y, z, w};
@@ -20,7 +17,7 @@ std::vector<int> count(int l, int r){
 
 int diff(int l, int r){
 	printf("diff %d %d\n", l, r);
-	std::cout << std::flush;
+	fflush(stdout);
 
 	int result = 0;
 	scanf("%d", &result);
@@ -32,6 +29,7 @@ bool check(int s, int e){
 	return ndif <= 0;
 }
 
+std::vector<std::pair<int, int>> p[101]{};
 int main(){
 	int t;
 	scanf("%d", &t);
@@ -40,7 +38,6 @@ int main(){
 		scanf("%d", &n);
 		if(n == -1) return 0;
 
-		std::vector<std::pair<int, int>> p{};
 		
 		int init = 0;
 		
@@ -53,18 +50,18 @@ int main(){
 			}
 			int cnt = 0;
 			for(auto x: count(init+1, no+1)) cnt += (x == 0);
-			p.push_back({init, std::min(n-1, yes + (cnt == 3))});
+			p[i].push_back({init, std::min(n-1, yes + (cnt == 3))});
 			init = yes+1+(cnt==3);
 		}
 
 		int result[4] = { 0, };
 		int idx = 0;
-		for(auto [l, r]: p){
+		for(auto [l, r]: p[i]){
 			result[idx] += r-l+1;
 			idx = (idx + 1) % 4;
 		}
-		printf("! %d %d %d %d\n", result[0], result[1], result[2], result[3]);
-		std::cout << std::flush;
+		printf("answer %d %d %d %d\n", result[0], result[1], result[2], result[3]);
+		fflush(stdout);
 	}
 	return 0;
 }

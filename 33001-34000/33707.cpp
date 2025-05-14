b52ec46342a71a2359854b4739dcb8e89559cbd6
bluefcat <bluefcat@gmail.com>
2025-05-14 14:11:15 +0900

    Add solve file 250514

--- 33001-34000/33707.cpp
+++ 33001-34000/33707.cpp
@@ -1,5 +1,4 @@
 #include <cstdio>
-#include <algorithm>
 
 int query(int r, int c){
 	printf("? %d %d\n", r, c);
@@ -12,12 +11,13 @@ int query(int r, int c){
 int main(){
 	int r, c;
 	scanf("%d %d", &r, &c);
-	int idx = (r & 1) && (c & 1);
-	int y = idx / c, x = idx % c;
-	while(query(y+1, x+1) != 1){
-		idx += 2;
-		y = idx / c, x = idx % c;
-	}
 
+	for(int y = 0; y < r; y ++){
+		for(
+			int x = 1 - (y&1); x < c; x +=2
+		){
+			if(query(y+1, x+1) == 1) return 0;
+		}
+	}
 	return 0;
 }

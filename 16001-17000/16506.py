b66c3595e317fa6923ec424a90dc692c5cfb10bf
bluefcat <bluefcat@gmail.com>
2025-05-29 10:43:34 +0900

    Add solve file in 250529

--- 16001-17000/16506.py
+++ 16001-17000/16506.py
@@ -1,8 +1,6 @@
 def f(o,p,q,r):
     c='C'in o
-    s="ADDSUBMOVANDORRNOTMUTLSLLSRASRRLLRRR"
-    o=s.find(o[:2]+o[-(1+c)])/3
-    o,p,q,r=map(lambda x:f"{bin(int(x))[2:]:0>3}",[o,p,q,r])
+    o,p,q,r=map(lambda x:f"{bin(int(x))[2:]:0>3}",["ADDSUBMOVANDORRNOTMUTLSLLSRASRRLLRRR".find(o[:2]+o[-1+c])/3,p,q,r])
     r=f"{r:0>{3+c}}"
     print(f"{o:0>4}{+c}0{p}{q}{r:0<4}")
 [f(*x.split())for x in[*open(0)][1:]]

191854ea2d2fb0daea4e56a8cfac23043e202526
bluefcat <bluefcat@gmail.com>
2025-06-02 11:22:47 +0900

    Add solve file in 250602

--- 1001-2000/1402.py
+++ 1001-2000/1402.py
@@ -1,26 +1,12 @@
 import sys 
 input = sys.stdin.readline
-
-def factorize(x):
-    result = []
-    for i in range(2, int(x**0.5)+1):
-        while x % i == 0:
-            result.append(i)
-            x //= i
-    if x != 1:
-        result.append(x)
-    return result
-
 t = int(input())
 
 for _ in range(t):
     a, b = map(int, input().split())
-    if a <= b:
-        print("yes")
-
-    elif sum(factorize(a)) <= b:
-        print("yes")
-    
-    else:
+    if a == 0 and b != 0:
         print("no")
+
+    else:
+        print("yes")
     

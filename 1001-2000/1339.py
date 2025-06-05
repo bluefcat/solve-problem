36624a4bfbdccc9da84ea76696101b74e4c05c49
bluefcat <bluefcat@gmail.com>
2025-06-05 13:47:08 +0900

    Add solve file in 250605

--- 1001-2000/1339.py
+++ 1001-2000/1339.py
@@ -4,38 +4,22 @@ n = sorted(
     key= lambda x: len(x)
 )
 
-mask = {chr(k): -1 for k in range(ord('A'), ord('A')+10)}
-app = {chr(k): 0 for k in range(ord('A'), ord('A')+10)}
+m = {k: -1 for k in range(ord('A'), ord('A')+27)}
+c = {k: 0 for k in range(ord('A'), ord('A')+27)}
+a = {k: 0 for k in range(ord('A'), ord('A')+27)}
+
 for x in n:
     for i, w in enumerate(x):
-        app[w] += 1*(10**i)
-
-
-c = []
-for i, _ in enumerate(max(n, key=lambda x: len(x))):
-    tmp = Counter([s[i] for s in n if len(s) > i])
-    c.append(tmp)
+        c[ord(w)] = 1
+        a[ord(w)] += (10**i)
 
 cand = 9
+while True:
+    w = max(m.keys(), key=lambda x: a[x]*c[x])
+    if c[w] == 0:
+        break
+    m[w], c[w] = cand, 0
+    cand -= 1
 
-for cnt in reversed(c):
-    while True:
-        p, pc = [], 0
-        for k, v in cnt.items():
-            if pc < v:
-                p, pc = [k], v
-            elif pc == v:
-                p.append(k)
-        
-        t = max(p, key=lambda x:app[x])
-
-        if cnt[t] == 0:
-            break
-        if mask[t] != -1:
-            cnt[t]=0;
-            continue
-        mask[t] = cand 
-        cand -= 1
-        cnt[t]=0
-print(sum(sum(mask[w] * (10**i) for i, w in enumerate(x)) for x in n))
+print(sum(sum(m[ord(w)] * (10**i) for i, w in enumerate(x)) for x in n))
 

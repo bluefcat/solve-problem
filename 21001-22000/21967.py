17231bebe038c2e2429b3adbb3eb923e9186c25b
bluefcat <bluefcat@gmail.com>
2025-06-09 09:44:48 +0900

    Add solve file in 250609

--- 21001-22000/21967.py
+++ 21001-22000/21967.py
@@ -2,13 +2,21 @@ import sys
 input = sys.stdin.readline
 _ = input()
 arr = [*map(int, input().split())]
+cnt = [0 for _ in range(11)]
 l, r, result = 0, 0, 0
+cnt[arr[l]] += 1
 
-while r < len(arr):
-    if abs(arr[r] - arr[l]) <= 2:
+while True:
+    tmp = [i for i, k in enumerate(cnt) if k != 0]
+    if max(tmp) - min(tmp) <= 2:
         result = max(result, r-l+1)
         r += 1 
+        if r >= len(arr):
+            break
+        cnt[arr[r]] += 1
     else:
-        l += 1
+        cnt[arr[l]] -= 1
+        l += 1 
+
 
 print(result)

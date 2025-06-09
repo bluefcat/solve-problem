58389ffd4a296a0f5d7ce6e35f06f7e10fe277d0
bluefcat <bluefcat@gmail.com>
2025-06-09 15:24:00 +0900

    Add solve file in 250609

--- 11001-12000/11385.cpp
+++ 11001-12000/11385.cpp
@@ -1,43 +1,62 @@
 #include <cstdio>
 #include <vector>
-#include <complex>
 
 using std::vector;
-using complex = std::complex<long double>;
-constexpr int N = 1000001;
-constexpr long double PI = 3.141592653589;
+using lint = long long;
 
-void fft(vector<complex>& f, bool inv){
-	int n = f.size();
+//3'221'225'473
+//const lint A = 3, B = 30, P = (A << B) | 1, R = 5; 
+//2'281'701'377
+//const lint A = 17, B = 27, P = (A << B) | 1, R = 3; 
+//2'013'265'921
+//const lint A = 15, B = 27, P = (A << B) | 1, R = 31; 
+//469'762'049
+//const lint A = 7, B = 26, P = (A << B) | 1, R = 3; 
+//998'244'353
+const lint A = 119, B = 23, P = (A << B) | 1, R = 3;
+
+lint pow(lint x, lint e, lint mod = P){
+	lint result = 1;
+	while(e){
+		if(e & 1) result = (result * x) % mod;
+		x = (x * x) % mod;
+		e >>= 1;
+	}
+	return result;
+}
+
+void ntt(vector<lint>& f, bool inv = false){
+	lint n = f.size();
 	if(n == 1) return;
 
-	for(int i = 1, j = 0; i < n; i ++){
-		int bit = n / 2;
+	for(lint i = 1, j = 0; i < n; i ++){
+		lint bit = n >> 1;
+		for(;j&bit; bit >>= 1) j^=bit;
+		j ^= bit;
 
-		while(j >= bit){
-			j -= bit;
-			bit /= 2;
-		}
-
-		j += bit;
 		if(i < j) std::swap(f[i], f[j]);
 	}
 
-	for(int k = 1; k < n; k <<= 1){
-		double angle = inv?PI/k:-PI/k;
-		complex w{cos(angle), sin(angle)};
+	for(lint k = 1; k < n; k <<= 1){
+		lint w = pow(inv?pow(R,P-2):R, P / k >> 1);
 
-		for(int i = 0; i < n; i += (k << 1)){
-			complex wi{1, 0};
+		for(lint i = 0; i < n; i += (k << 1)){
+			lint wi=1;
 
-			for(int j = 0; j < k; j ++){
-				complex even = f[i+j];
-				complex odd = f[i+j+k];
-				f[i+j] = even + wi * odd;
-				f[i+j+k] = even - wi * odd;
-				wi *= w;
+			for(lint j = 0; j < k; j ++){
+				lint even = f[i+j];
+				lint odd = (f[i+j+k] * wi) % P;
+				f[i+j] = (even + odd) % P;
+				f[i+j+k] = (even - odd + P) % P; 
+				wi = (wi*w) % P;
 			}
 		}
+	}
+	if(inv){
+		lint ip = pow(n, P-2);
+		//M^-1 = 1/n * (M that w has negative angle)
+		for(lint& x: f)
+			x = (x * ip) % P;
 	}
 }
 
@@ -51,10 +70,8 @@ void print(__int128 x){
 }
 
 int main(){
-	int size = 1;	
-	while(N+2 > size) size <<= 1;
-	size <<= 1;
-	vector<complex> p(size, 0), q(size, 0);
+	int size = (1 << 20);	
+	vector<lint> p(size, 0), q(size, 0);
 	int n, m;
 	scanf("%d %d", &n, &m);
 	for(int i = 0; i < n+1; i ++){
@@ -68,17 +85,15 @@ int main(){
 		q[i] = x;
 	}
 	
-	fft(p, false);
-	fft(q, false);
-	for(int i = 0; i < size; i ++) p[i] *= q[i];
-	fft(p, true);
+	ntt(p, false);
+	ntt(q, false);
+	for(int i = 0; i < size; i ++) p[i] = (p[i] * q[i]) % P;
+	ntt(p, true);
 	
 	__int128 result = 0;
 
 	for(int i = 0; i < (n+1)*(m+1); i ++){
-		p[i] /= complex(size, 0);
-		p[i] = complex(std::round(p[i].real()), std::round(p[i].imag()));
-		result ^= (__int128)p[i].real();
+		result ^= p[i];
 	}
 	print(result);
 	printf("\n");

e19175d85ca69722419b9b789bc5e7a34873801e
bluefcat <bluefcat@gmail.com>
2025-06-11 15:55:37 +0900

    Add solve file in 250611

---
+++ 28001-29000/28278.py
@@ -0,0 +1,11 @@
+f={
+    1:lambda k,v: k.append(v),
+    2:lambda k:print(k and k.pop()or -1),
+    3:lambda k:print(len(k)),
+    4:lambda k:print(+(not k)),
+    5:lambda k:print(k and k[0]or -1),
+}
+s=[]
+for _ in '*'*int(input()):
+    k,*v=map(int,input().split())
+    f[k](s,*v)

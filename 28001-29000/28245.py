001ad04d9264b8f56125f0c27d0b668fcf610394
bluefcat <bluefcat@gmail.com>
2025-06-18 16:08:40 +0900

    Add solve file in 250618

--- 28001-29000/28245.py
+++ 28001-29000/28245.py
@@ -5,8 +5,8 @@ def f(x):
     a, b =0, 0
     t=[]
     r = 999999999
-    for i in range(0, 20):
-        for j in range(i, 20):
+    for i in range(0, 100):
+        for j in range(i, 100):
             k = 2**i+2**j
             if r >= abs(x - k):
                 if r > abs(x-k):
@@ -20,26 +20,23 @@ def f(x):
 
 for _ in "*"*int(input()):
     m=int(input())
-    x=[i for i,r in enumerate(bin(m)[2:][::-1]) if r == '1'][-2:]
-    if len(x) == 1:
-        if m <= 2:
-            print(0, 0)
-        else:
-            print(x[-1]-1, x[-1]-1)
+    if m == 1:
+        print(0, 0)
         continue
+    b, b1 = 1,0
+    while b < m:
+        b *= 2
+        b1 += 1
 
-    a=2**(x[1]+1)
-    b=2**(x[0]+1)
-    k=2**x[0]+2**x[1]
-    t = sorted([
-            (abs(m-k), k, (x[0], x[1])),
-            (abs(m-a), a, (x[1], x[1])),
-            (abs(m-b), b, (x[0], x[0]))
-            ], 
-           key= lambda x: (x[0], x[1])
-    )[0][2]
+    x=[i for i,r in enumerate(bin(m)[2:][::-1]) if r == '1'][-2:]
+    x1 = 2**x[0] + 2**x[-1]    
+
+    if abs(x1-m) <= abs(b-m):
+        print(x[0], x[1])
+        continue 
+    print(b1-1, b1-1)
 
 
 
-    print(*t)
+
 

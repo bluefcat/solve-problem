a8f61307fe74b72b103b309fe21f76bbd597c60e
bluefcat <bluefcat@gmail.com>
2025-07-10 14:15:34 +0900

    Add solve file in 250710

--- 16001-17000/16174.cpp
+++ 16001-17000/16174.cpp
@@ -2,15 +2,23 @@
 
 constexpr int N = 65;
 int board[N][N] = {};
+bool visit[N][N] = {};
 bool win = false;
 
 void solve(int n, int x, int y){
-	if(board[x][y] == -1) win = true;
-	if(win) return;
+	if(board[y][x] == -1){ 
+		win = true;
+	}
 	
-	int d = board[x][y];
-	if(0 <= x+d && x+d < n) solve(n, x+d, y);
-	if(0 <= y+d && y+d < n) solve(n, x, y+d);
+	int d = board[y][x];
+	if(0 <= x+d && x+d < n && !visit[y][x+d] && !win){ 
+		visit[y][x+d] = true;
+		solve(n, x+d, y);
+	}
+	if(0 <= y+d && y+d < n && !visit[y+d][x] && !win){ 
+		visit[y+d][x] = true;
+		solve(n, x, y+d);
+	}
 	return;
 }
 
@@ -21,7 +29,10 @@ int main(){
 	for(int y = 0; y < n; y ++)
 		for(int x = 0; x < n; x ++)
 			scanf("%d", board[y] + x);
+
+	visit[0][0] = true;
 	solve(n, 0, 0);
+
 	if(win){
 		printf("HaruHaru\n");
 		return 0;
